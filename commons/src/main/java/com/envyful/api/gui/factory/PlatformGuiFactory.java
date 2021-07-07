259bea9405ec8bc47110fe16fee3b7b3849c3c7e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-07 10:17:27 +0100

    Feat: created PlatformGuiFactory interface

---
+++ commons/src/main/java/com/envyful/api/gui/factory/PlatformGuiFactory.java
@@ -0,0 +1,38 @@
+package com.envyful.api.gui.factory;
+
+import com.envyful.api.gui.Gui;
+import com.envyful.api.gui.page.Pane;
+import com.envyful.api.gui.page.type.PagedPane;
+
+/**
+ *
+ * Factory interface for handling creating the platform specific builder implementations
+ *
+ */
+public interface PlatformGuiFactory {
+
+    /**
+     *
+     * Gets the platform's {@link Pane.Builder} class as a new instance
+     *
+     * @return The pane builder for the given platform
+     */
+    Pane.Builder paneBuilder();
+
+    /**
+     *
+     * Gets the platform's {@link PagedPane.Builder} class as a new instance
+     *
+     * @return The paged pane builder for the given platform
+     */
+    PagedPane.Builder pagedPaneBuilder();
+
+    /**
+     *
+     * Gets the platform's {@link Gui.Builder} class as a new instance
+     *
+     * @return The gui builder for the given platform
+     */
+    Gui.Builder guiBuilder();
+
+}

ec693106298e2cfbbfbae5fb95897b857492b82d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-20 00:02:36 +0100

    Fix: formatting for pages

--- forge/src/main/java/com/envyful/api/forge/chat/PageBuilder.java
+++ forge/src/main/java/com/envyful/api/forge/chat/PageBuilder.java
@@ -15,6 +15,8 @@ import java.util.function.Function;
  */
 public class PageBuilder<T> {
 
+    private String mainColor = "§e";
+    private String offColor = "§e";
     private int pageSize = 5;
     private List<T> values = Lists.newArrayList();
     private Function<T, String> converter = null;
@@ -69,6 +71,30 @@ public class PageBuilder<T> {
 
     /**
      *
+     * Sets the main colour for the formatting
+     *
+     * @param mainColor The main colour
+     * @return The builder
+     */
+    public PageBuilder<T> mainColor(String mainColor) {
+        this.mainColor = mainColor;
+        return this;
+    }
+
+    /**
+     *
+     * Sets the off colour for the formatting
+     *
+     * @param offColor The off colour
+     * @return The builder
+     */
+    public PageBuilder<T> offColor(String offColor) {
+        this.offColor = offColor;
+        return this;
+    }
+
+    /**
+     *
      * Sends the given page to the user
      *
      * @param sender The person to send the page to
@@ -80,7 +106,7 @@ public class PageBuilder<T> {
                 break;
             }
 
-            sender.sendMessage(new TextComponentString(this.converter.apply(this.values.get(i))));
+            sender.sendMessage(new TextComponentString(this.mainColor + i + ". " + this.offColor + this.converter.apply(this.values.get(i))));
         }
     }
 }

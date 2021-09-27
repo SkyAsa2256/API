60468b1a457c47d15c83f329b717071a3b99e0cb
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-09-27 13:41:17 +0100

    fix(api): remove direct class itself

--- commons/src/main/java/com/envyful/api/command/annotate/executor/Completable.java
+++ commons/src/main/java/com/envyful/api/command/annotate/executor/Completable.java
@@ -11,6 +11,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Completable {
 
-    Class<TabCompleter> value();
+    Class<? extends TabCompleter<?, ?>> value();
 
 }

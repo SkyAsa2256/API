455918491902dd71fd3b03d23d85d4abc9ff224f
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-10 21:55:28 +0100

    feat(api): add else item
    skip-ci

--- commons/src/main/java/com/envyful/api/config/type/PermissibleConfigItem.java
+++ commons/src/main/java/com/envyful/api/config/type/PermissibleConfigItem.java
@@ -9,19 +9,25 @@ import java.util.Map;
 public class PermissibleConfigItem extends PositionableConfigItem {
 
     private String permission;
+    private ConfigItem elseItem;
 
     public PermissibleConfigItem() {
         super();
     }
 
     public PermissibleConfigItem(String type, int amount, byte damage, String name, List<String> lore, int xPos,
-                                 int yPos, String permission, Map<String, NBTValue> nbt) {
+                                 int yPos, String permission, Map<String, NBTValue> nbt, ConfigItem elseItem) {
         super(type, amount, damage, name, lore, xPos, yPos, nbt);
 
         this.permission = permission;
+        this.elseItem = elseItem;
     }
 
     public String getPermission() {
         return this.permission;
+    }
+
+    public ConfigItem getElseItem() {
+        return this.elseItem;
     }
 }

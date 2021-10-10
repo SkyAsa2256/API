5b7eef58b0bd821c7e8a3a28e72e10855e9f8b1d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-10 22:25:21 +0100

    feat(forge): add more utility methods
    skip-ci

--- forge/src/main/java/com/envyful/api/forge/config/UtilConfigItem.java
+++ forge/src/main/java/com/envyful/api/forge/config/UtilConfigItem.java
@@ -2,17 +2,58 @@ package com.envyful.api.forge.config;
 
 import com.envyful.api.config.type.ConfigItem;
 import com.envyful.api.config.type.PermissibleConfigItem;
+import com.envyful.api.config.type.PositionableConfigItem;
 import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.api.forge.items.ItemBuilder;
 import com.envyful.api.forge.player.util.UtilPlayer;
+import com.envyful.api.gui.factory.GuiFactory;
+import com.envyful.api.gui.item.Displayable;
+import com.envyful.api.gui.pane.Pane;
+import com.envyful.api.player.EnvyPlayer;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.*;
 
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class UtilConfigItem {
+
+    public static void addPermissibleConfigItem(Pane pane, EntityPlayerMP player, PermissibleConfigItem configItem) {
+        addPermissibleConfigItem(pane, player, configItem, null);
+    }
+
+    public static void addPermissibleConfigItem(Pane pane, EntityPlayerMP player, PermissibleConfigItem configItem,
+                                                BiConsumer<EnvyPlayer<?>, Displayable.ClickType> clickHandler) {
+        ItemStack itemStack = fromPermissibleItem(player, configItem);
+
+        if (itemStack == null) {
+            return;
+        }
+
+        if (clickHandler == null) {
+            pane.set(configItem.getXPos(), configItem.getYPos(), GuiFactory.displayable(itemStack));
+        } else {
+            pane.set(configItem.getXPos(), configItem.getYPos(), GuiFactory.displayableBuilder(itemStack)
+                    .clickHandler(clickHandler).build());
+        }
+    }
+
+    public static void addConfigItem(Pane pane, PositionableConfigItem configItem) {
+        addConfigItem(pane, configItem, null);
+    }
+
+    public static void addConfigItem(Pane pane, PositionableConfigItem configItem,
+                                     BiConsumer<EnvyPlayer<?>, Displayable.ClickType> clickHandler) {
+        if (clickHandler == null) {
+            pane.set(configItem.getXPos(), configItem.getYPos(), GuiFactory.displayable(fromConfigItem(configItem)));
+        } else {
+            pane.set(configItem.getXPos(), configItem.getYPos(), GuiFactory.displayableBuilder(fromConfigItem(configItem))
+                    .clickHandler(clickHandler).build());
+        }
+    }
 
     public static ItemStack fromPermissibleItem(EntityPlayerMP player, PermissibleConfigItem permissibleConfigItem) {
         if (!permissibleConfigItem.isEnabled()) {

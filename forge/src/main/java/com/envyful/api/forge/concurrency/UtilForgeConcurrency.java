5046a745e58da7c0233d571c4f63ceab818bab72
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-18 20:09:26 +0100

    feat(forge): add UtilForgeConcurrency#runWhenTrue
    skip-ci

--- forge/src/main/java/com/envyful/api/forge/concurrency/UtilForgeConcurrency.java
+++ forge/src/main/java/com/envyful/api/forge/concurrency/UtilForgeConcurrency.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Predicate;
 
 /**
  *
@@ -27,6 +28,26 @@ public class UtilForgeConcurrency {
      * @param runnable The runnable to be run on the main thread
      */
     public static void runSync(Runnable runnable) {
+        TICK_LISTENER.addTask(runnable);
+    }
+
+    /**
+     *
+     * Executes the runnable task the tick after the predicate returns true
+     *
+     * @param predicate The predicate to use
+     * @param runnable The runnable to execute
+     */
+    public static void runWhenTrue(Predicate<Runnable> predicate, Runnable runnable) {
+        TICK_LISTENER.addTask(() -> attemptRun(predicate, runnable));
+    }
+
+    private static void attemptRun(Predicate<Runnable> predicate, Runnable runnable) {
+        if (!predicate.test(runnable)) {
+            TICK_LISTENER.addTask(() -> attemptRun(predicate, runnable));
+            return;
+        }
+
         TICK_LISTENER.addTask(runnable);
     }
 }

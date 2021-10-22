add2d7910a25faa70b25bf2cb6e68c1374760a4d
Daniel Norris <dnorris443@gmail.com>
2021-10-22 17:37:04 +0100

    feat(reforged): add UtilPokemonPrice
    skip-ci

---
+++ reforged/src/main/java/com/envyful/api/reforged/pixelmon/config/UtilPokemonPrice.java
@@ -0,0 +1,20 @@
+package com.envyful.api.reforged.pixelmon.config;
+
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+
+import java.util.List;
+
+public class UtilPokemonPrice {
+
+    public static double getMinPrice(Pokemon pokemon, double defaultPrice, List<PokeSpecPricing> modifiers) {
+        double currentPrice = defaultPrice;
+
+        for (PokeSpecPricing minPriceModifier : modifiers) {
+            if (minPriceModifier.getSpec().matches(pokemon)) {
+                currentPrice = minPriceModifier.apply(currentPrice);
+            }
+        }
+
+        return currentPrice;
+    }
+}

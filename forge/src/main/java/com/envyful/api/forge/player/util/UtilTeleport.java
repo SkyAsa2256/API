175a179137f60f0c15bccfe05c326d7f09ef9988
Daniel Norris <dnorris443@gmail.com>
2021-10-23 13:58:51 +0100

    fix(forge): correct dimension teleporter
    skip-ci

--- forge/src/main/java/com/envyful/api/forge/player/util/UtilTeleport.java
+++ forge/src/main/java/com/envyful/api/forge/player/util/UtilTeleport.java
@@ -1,11 +1,14 @@
 package com.envyful.api.forge.player.util;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.management.PlayerList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 /**
@@ -29,9 +32,23 @@ public class UtilTeleport {
      */
     public static void teleportPlayer(EntityPlayerMP player, World world, Vec3d pos, float pitch, float yaw) {
         if (player.getServerWorld().provider.getDimension() != world.provider.getDimension()) {
-            PLAYER_LIST.transferPlayerToDimension(player, world.provider.getDimension(), new Teleporter((WorldServer) world));
+            PLAYER_LIST.transferPlayerToDimension(player, world.provider.getDimension(), new PlayerTeleporter(new BlockPos(pos)));
         }
 
         player.connection.setPlayerLocation(pos.x, pos.y, pos.z, yaw, pitch);
+    }
+
+    private static class PlayerTeleporter implements ITeleporter {
+
+        private final BlockPos targetPos;
+
+        private PlayerTeleporter(BlockPos targetPos) {
+            this.targetPos = targetPos;
+        }
+
+        @Override
+        public void placeEntity(World world, Entity entity, float yaw) {
+            entity.moveToBlockPosAndAngles(targetPos, yaw, entity.rotationPitch);
+        }
     }
 }

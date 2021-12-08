8fc7c0c4baba3ce7240d2cbf5db563b89d0cc5d7
Daniel Norris <dnorris443@gmail.com>
2021-12-08 21:53:47 +0000

    fix(forge): add missing continue
    skip-ci

--- forge/src/main/java/com/envyful/api/forge/command/command/executor/CommandExecutor.java
+++ forge/src/main/java/com/envyful/api/forge/command/command/executor/CommandExecutor.java
@@ -201,6 +201,7 @@ public class CommandExecutor {
                         return false;
                     } else {
                         ++subtract;
+                        continue;
                     }
                 }
 

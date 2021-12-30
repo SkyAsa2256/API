c4eb799ca59a53ef019c599aa08859a31dd10bcc
danorris709 <dnorris443@gmail.com>
2021-12-30 16:37:00 +0000

    feat(forge16): add util forge server
    skip-ci

---
+++ forge16/src/main/java/com/envyful/api/forge/server/UtilForgeServer.java
@@ -0,0 +1,26 @@
+package com.envyful.api.forge.server;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
+/**
+ *
+ * Static utility class for handling forge server function
+ *
+ */
+public class UtilForgeServer {
+
+    private static final MinecraftServer SERVER = ServerLifecycleHooks.getCurrentServer();
+
+    /**
+     *
+     * Executes the given command from the server
+     *
+     * Ensure to set the server first
+     *
+     * @param command The command to execyte
+     */
+    public static void executeCommand(String command) {
+        SERVER.getCommandManager().handleCommand(SERVER.getCommandSource(), command);
+    }
+}

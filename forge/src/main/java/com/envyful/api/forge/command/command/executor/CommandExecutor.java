c32e09e880e29faf81836b1eed0695561f056c6b
Daniel Norris <dnorris443@gmail.com>
2022-04-19 22:23:06 +0100

    2.1.7

--- forge/src/main/java/com/envyful/api/forge/command/command/executor/CommandExecutor.java
+++ forge/src/main/java/com/envyful/api/forge/command/command/executor/CommandExecutor.java
@@ -286,16 +286,16 @@ public class CommandExecutor {
         A castSender = completer.getSenderClass().cast(sender);
         List<String> completions = completer.getCompletions(castSender, args, this.extraTabData.get(pos));
 
-        System.out.println(this.arguments[pos + 1] + " " + this.arguments[pos + 1].getY());
-        if (this.arguments[pos + 1] != null && this.arguments[pos + 1].getY() != null) {
+        System.out.println(this.arguments[pos] + " " + this.arguments[pos + 1].getY());
+        System.out.println(this.arguments[pos] + " " + this.arguments[pos].getY());
+        if (this.arguments[pos] != null && this.arguments[pos].getY() != null) {
             System.out.println(this.tabCompleters.size() + " " + (pos + 1));
-            if (this.tabCompleters.size() > (pos + 2)) {
-                TabCompleter<?, A> tabCompleter = (TabCompleter<?, A>) this.tabCompleters.get(pos + 2);
+            if (this.tabCompleters.size() > (pos + 1)) {
+                TabCompleter<?, A> tabCompleter = (TabCompleter<?, A>) this.tabCompleters.get(pos + 1);
 
                 System.out.println(tabCompleter + " HELLO");
                 if (!(completer instanceof FillerTabCompleter)) {
-                    System.out.println("FILLER");
-                    completions.addAll(tabCompleter.getCompletions(castSender, args, this.extraTabData.get(pos + 2)));
+                    completions.addAll(tabCompleter.getCompletions(castSender, args, this.extraTabData.get(pos + 1)));
                 }
             }
         }

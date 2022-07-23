95a222db8e3d34b8796c5148a0ea2197ee0d71d1
Daniel Norris <dnorris443@gmail.com>
2022-07-23 02:15:25 +0100

    feat(reforged16): add nickname placeholder to sprite config

--- reforged16/src/main/java/com/envyful/api/reforged/pixelmon/sprite/UtilSprite.java
+++ reforged16/src/main/java/com/envyful/api/reforged/pixelmon/sprite/UtilSprite.java
@@ -103,6 +103,7 @@ public class UtilSprite {
         ExtraStats extraStats = pokemon.getExtraStats();
 
         line = line
+                .replace("%nickname%", pokemon.getDisplayName())
                 .replace("%palette%", pokemon.getPalette().getLocalizedName())
                 .replace("%species_name%", pokemon.getSpecies().getLocalizedName())
                 .replace("%level%", pokemon.getPokemonLevel() + "")

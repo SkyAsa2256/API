af84d0207814e04f41cc9f35a8a7b7e6d0e8b2e6
Daniel Norris <dnorris443@gmail.com>
2022-08-24 00:27:02 +0100

    3.1.1

--- reforged16/src/main/java/com/envyful/api/reforged/pixelmon/PokemonSpec.java
+++ reforged16/src/main/java/com/envyful/api/reforged/pixelmon/PokemonSpec.java
@@ -3,7 +3,6 @@ package com.envyful.api.reforged.pixelmon;
 import com.envyful.api.forge.items.UtilItemStack;
 import com.envyful.api.type.requirement.Requirement;
 import com.google.common.collect.Lists;
-import com.pixelmonmod.api.pokemon.PokemonSpecification;
 import com.pixelmonmod.pixelmon.api.pokemon.Nature;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.api.pokemon.species.Species;
@@ -184,8 +183,8 @@ public class PokemonSpec {
 
         if (this.allowEvolutions) {
             if (!Objects.equals(this.species, pokemon.getSpecies())) {
-                for (PokemonSpecification preEvolution : pokemon.getForm().getBaseEvolution().getForm().getPreEvolutions()) {
-                    if (preEvolution.matches(pokemon)) {
+                for (Species preEvolution : pokemon.getForm().getPreEvolutions()) {
+                    if (Objects.equals(pokemon.getSpecies(), preEvolution)) {
                         return true;
                     }
                 }

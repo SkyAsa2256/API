5e9da72192a149ed185ec09a6b48995ede7fc717
Daniel Norris <dnorris443@gmail.com>
2022-09-15 16:41:06 +0100

    feat(forge16): add UtilWorld#getRandomPosition
    skip-ci

--- forge16/src/main/java/com/envyful/api/forge/world/UtilWorld.java
+++ forge16/src/main/java/com/envyful/api/forge/world/UtilWorld.java
@@ -1,6 +1,9 @@
 package com.envyful.api.forge.world;
 
+import com.envyful.api.math.UtilRandom;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.storage.ServerWorldInfo;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
@@ -11,6 +14,20 @@ import net.minecraftforge.fml.server.ServerLifecycleHooks;
  *
  */
 public class UtilWorld {
+
+    public static BlockPos getRandomPosition(World world, int radius) {
+        return getRandomPosition(world, radius, radius);
+    }
+
+    public static BlockPos getRandomPosition(World world, int radiusX, int radiusZ) {
+        BlockPos pos = new BlockPos(
+                (UtilRandom.randomBoolean() ? 1 : -1) * UtilRandom.randomInteger(0, radiusX),
+                0,
+                (UtilRandom.randomBoolean() ? 1 : -1) * UtilRandom.randomInteger(0, radiusZ));
+
+        int y = world.getHeight(Heightmap.Type.MOTION_BLOCKING, pos.getX(), pos.getZ());
+        return new BlockPos(pos.getX(), y, pos.getZ());
+    }
 
     /**
      *

9ba48e2088debff2f34373c40aa166a23c0c8dbc
Daniel Norris <dnorris443@gmail.com>
2022-12-01 06:49:23 +0000

    feat(reforged16): add DialogueInputRegistry
    skip-ci

---
+++ reforged16/src/main/java/com/envyful/api/reforged/dialogue/DialogueInputRegistry.java
@@ -0,0 +1,103 @@
+package com.envyful.api.reforged.dialogue;
+
+import com.google.common.collect.Maps;
+import com.pixelmonmod.pixelmon.Pixelmon;
+import com.pixelmonmod.pixelmon.api.events.dialogue.DialogueInputEvent;
+import com.pixelmonmod.pixelmon.api.util.helpers.NetworkHelper;
+import com.pixelmonmod.pixelmon.comm.packetHandlers.dialogue.OpenDialogueInputPacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class DialogueInputRegistry {
+
+    private static final Map<UUID, Builder> REGISTERED_DIALOGUES = Maps.newConcurrentMap();
+
+    static {
+        Pixelmon.EVENT_BUS.register(DialogueInputRegistry.class);
+    }
+
+    private DialogueInputRegistry() {
+        throw new UnsupportedOperationException("Invalid usage - this is a static registry");
+    }
+
+    private static void addDialogue(ServerPlayerEntity player, Builder builder) {
+        REGISTERED_DIALOGUES.put(player.getUUID(), builder);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @SubscribeEvent
+    public static void onDialogueSubmit(DialogueInputEvent.Submitted event) {
+        Builder builder = REGISTERED_DIALOGUES.get(event.getPlayer().getUUID());
+
+        if (builder != null && builder.submitHandler != null) {
+            builder.submitHandler.accept(event);
+        }
+
+        REGISTERED_DIALOGUES.remove(event.getPlayer().getUUID());
+    }
+
+    @SubscribeEvent
+    public static void onDialogueSubmit(DialogueInputEvent.ClosedScreen event) {
+        Builder builder = REGISTERED_DIALOGUES.get(event.getPlayer().getUUID());
+
+        if (builder != null && builder.closeHandler != null) {
+            builder.closeHandler.accept(event);
+        }
+
+        REGISTERED_DIALOGUES.remove(event.getPlayer().getUUID());
+    }
+
+    public static class Builder {
+
+        private ITextComponent title;
+        private ITextComponent text;
+        private boolean closeOnEsc = true;
+        private Consumer<DialogueInputEvent.Submitted> submitHandler;
+        private Consumer<DialogueInputEvent.ClosedScreen> closeHandler;
+
+        public Builder title(ITextComponent title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder text(ITextComponent text) {
+            this.text = text;
+            return this;
+        }
+
+        public Builder closeOnEscape() {
+            this.closeOnEsc = true;
+            return this;
+        }
+
+        public Builder notCloseable() {
+            this.closeOnEsc = false;
+            return this;
+        }
+
+        public Builder submitHandler(Consumer<DialogueInputEvent.Submitted> submitHandler) {
+            this.submitHandler = submitHandler;
+            return this;
+        }
+
+        public Builder closeHandler(Consumer<DialogueInputEvent.ClosedScreen> closeHandler) {
+            this.closeHandler = closeHandler;
+            return this;
+        }
+
+        public void open(ServerPlayerEntity player) {
+            NetworkHelper.sendPacket(player, new OpenDialogueInputPacket(
+                    this.title, this.text, this.text.getString(), this.closeOnEsc
+            ));
+            DialogueInputRegistry.addDialogue(player, this);
+        }
+    }
+}

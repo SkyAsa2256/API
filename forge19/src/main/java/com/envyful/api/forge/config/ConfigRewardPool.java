5c09d0bdb93cdd686e1e3baa04cf75b7f8c995ab
Daniel Norris <dnorris443@gmail.com>
2022-12-19 19:09:37 +0000

    fix(forge16/19): make ConfigRewardPool use ConfigRandomWeightedSet
    skip-ci

--- forge19/src/main/java/com/envyful/api/forge/config/ConfigRewardPool.java
+++ forge19/src/main/java/com/envyful/api/forge/config/ConfigRewardPool.java
@@ -1,7 +1,7 @@
 package com.envyful.api.forge.config;
 
+import com.envyful.api.config.type.ConfigRandomWeightedSet;
 import com.envyful.api.gui.Transformer;
-import com.envyful.api.math.RandomWeightedSet;
 import com.envyful.api.math.UtilRandom;
 import com.google.common.collect.Lists;
 import net.minecraft.server.level.ServerPlayer;
@@ -15,9 +15,9 @@ public class ConfigRewardPool {
     private ConfigReward guaranteedReward;
     private int rewardRollsMin;
     private int rewardRollsMax;
-    private RandomWeightedSet<ConfigReward> rewards;
+    private ConfigRandomWeightedSet<ConfigReward> rewards;
 
-    public ConfigRewardPool(ConfigReward guaranteedReward, int rewardRollsMin, int rewardRollsMax, RandomWeightedSet<ConfigReward> rewards) {
+    private ConfigRewardPool(ConfigReward guaranteedReward, int rewardRollsMin, int rewardRollsMax, ConfigRandomWeightedSet<ConfigReward> rewards) {
         this.guaranteedReward = guaranteedReward;
         this.rewardRollsMin = rewardRollsMin;
         this.rewardRollsMax = rewardRollsMax;
@@ -44,6 +44,47 @@ public class ConfigRewardPool {
 
         for (ConfigReward randomReward : this.getRandomRewards()) {
             randomReward.execute(player, transformers);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private ConfigReward guaranteedReward;
+        private int rewardRollsMin;
+        private int rewardRollsMax;
+        private ConfigRandomWeightedSet<ConfigReward> rewards;
+
+        private Builder() {}
+
+        public Builder guranteedReward(ConfigReward reward) {
+            this.guaranteedReward = reward;
+            return this;
+        }
+
+        public Builder minRolls(int minRolls) {
+            this.rewardRollsMin = minRolls;
+            return this;
+        }
+
+        public Builder maxRolls(int maxRolls) {
+            this.rewardRollsMax = maxRolls;
+            return this;
+        }
+
+        public Builder rewards(ConfigRandomWeightedSet<ConfigReward> rewards) {
+            this.rewards = rewards;
+            return this;
+        }
+
+        public ConfigRewardPool build() {
+            return new ConfigRewardPool(
+                    this.guaranteedReward, this.rewardRollsMin, this.rewardRollsMax,
+                    this.rewards
+            );
         }
     }
 }

52939f0b133383a7b263e2848f90e20e79c2ce4a
Daniel Norris <dnorris443@gmail.com>
2023-01-06 21:52:35 +0000

    fix(discord): update JDA imports

--- discord/src/main/java/com/envyful/api/discord/component/ModalFactory.java
+++ discord/src/main/java/com/envyful/api/discord/component/ModalFactory.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.ItemComponent;
-import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.modals.Modal;
 
 import java.util.ArrayList;
 import java.util.List;

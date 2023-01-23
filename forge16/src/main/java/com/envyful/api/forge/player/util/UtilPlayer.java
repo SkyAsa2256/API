0a512d58c012e27084702ed817056fce722a985f
Daniel Norris <dnorris443@gmail.com>
2023-01-23 22:05:59 +0000

    chore(gradle): bump version to 4.1.2

--- forge16/src/main/java/com/envyful/api/forge/player/util/UtilPlayer.java
+++ forge16/src/main/java/com/envyful/api/forge/player/util/UtilPlayer.java
@@ -1,19 +1,11 @@
 package com.envyful.api.forge.player.util;
 
-import com.envyful.api.forge.player.ForgeEnvyPlayer;
-import com.google.common.collect.Sets;
-import io.netty.util.AttributeKey;
 import net.minecraft.command.ICommandSource;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.management.OpEntry;
-import net.minecraftforge.fml.network.FMLConnectionData;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import net.minecraftforge.server.permission.PermissionAPI;
 
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -22,17 +14,6 @@ import java.util.UUID;
  *
  */
 public class UtilPlayer {
-
-    private static AttributeKey<FMLConnectionData> ATTRIBUTE;
-
-    static {
-        try {
-            Field connectionData = FMLNetworkConstants.class.getDeclaredField("FML_CONNECTION_DATA");
-            ATTRIBUTE = (AttributeKey<FMLConnectionData>) connectionData.get(null);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static String getName(ICommandSource source) {
         if (source instanceof ServerPlayerEntity) {
@@ -84,37 +65,5 @@ public class UtilPlayer {
      */
     public static ServerPlayerEntity getOnlinePlayer(UUID uuid) {
         return ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayer(uuid);
-    }
-
-    /**
-     *
-     * Gets the player's client side mods
-     *
-     * @param player The player
-     * @return The mod names/ids
-     */
-    public static Set<String> getMods(ForgeEnvyPlayer player) {
-        if (player == null) {
-            return Collections.emptySet();
-        }
-
-        return getMods(player.getParent());
-    }
-
-    /**
-     *
-     * Gets the player's client side mods
-     *
-     * @param player The player
-     * @return The mod names/ids
-     */
-    public static Set<String> getMods(ServerPlayerEntity player) {
-        if (player == null || player.connection == null || player.connection.connection == null ||
-                player.connection.connection.channel() == null) {
-            return Collections.emptySet();
-        }
-
-        FMLConnectionData networkDispatcher = player.connection.connection.channel().attr(ATTRIBUTE).get();
-        return Sets.newHashSet(networkDispatcher.getModList());
     }
 }

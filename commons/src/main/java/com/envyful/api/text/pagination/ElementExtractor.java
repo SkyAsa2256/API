c60fc9d12c5b2aefb09e0bea7307b50fd10e343d
Daniel Norris <dnorris443@gmail.com>
2023-01-28 11:42:20 +0000

    feat(api): add Element extractor class

---
+++ commons/src/main/java/com/envyful/api/text/pagination/ElementExtractor.java
@@ -0,0 +1,34 @@
+package com.envyful.api.text.pagination;
+
+import com.google.common.collect.Lists;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ *
+ * Used for converting an iterable of the type into a list of Strings
+ *
+ * @param <T>
+ */
+public class ElementExtractor<T> {
+
+    private final Iterator<T> elements;
+    private final Function<T, String> extractor;
+
+    public ElementExtractor(Iterator<T> elements, Function<T, String> extractor) {
+        this.elements = elements;
+        this.extractor = extractor;
+    }
+
+    public List<String> extract() {
+        List<String> elements = Lists.newArrayList();
+
+        while (this.elements.hasNext()) {
+            elements.add(this.extractor.apply(this.elements.next()));
+        }
+
+        return elements;
+    }
+}

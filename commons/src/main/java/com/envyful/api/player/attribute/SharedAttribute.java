37fe456cc536512874bc08d6007ed0d04e67c92b
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2023-05-12 14:16:31 +0100

    fix(api): add player manager to default constructor for SharedAttribute

--- commons/src/main/java/com/envyful/api/player/attribute/SharedAttribute.java
+++ commons/src/main/java/com/envyful/api/player/attribute/SharedAttribute.java
@@ -1,5 +1,7 @@
 package com.envyful.api.player.attribute;
 
+import com.envyful.api.player.PlayerManager;
+
 /**
  *
  * Abstract implementation of the attribute class storing the manager
@@ -10,10 +12,14 @@ package com.envyful.api.player.attribute;
  */
 public abstract class SharedAttribute<A, B> extends AbstractAttribute<A, B> {
 
+    protected final transient PlayerManager<?, ?> playerManager;
+
     protected A id;
 
-    protected SharedAttribute(B manager) {
+    protected SharedAttribute(B manager, PlayerManager<?, ?> playerManager) {
         super(manager);
+
+        this.playerManager = playerManager;
     }
 
     @Override
@@ -31,6 +37,10 @@ public abstract class SharedAttribute<A, B> extends AbstractAttribute<A, B> {
     @Override
     public void save(A id) {
         this.id = id;
+
+        if (!this.shouldSave()) {
+            return;
+        }
 
         this.save();
     }

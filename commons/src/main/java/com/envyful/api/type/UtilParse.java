0f561c1bf17df4f78258915459490cff254c01e8
Daniel Norris <dnorris443@gmail.com>
2023-05-18 22:20:57 +0100

    ref(api): update UtilParse to use more efficient, and cleaner, methods for validating and deprecate parseInteger

--- commons/src/main/java/com/envyful/api/type/UtilParse.java
+++ commons/src/main/java/com/envyful/api/type/UtilParse.java
@@ -1,6 +1,11 @@
 package com.envyful.api.type;
 
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -9,6 +14,33 @@ import java.util.Optional;
  */
 public class UtilParse {
 
+    private static final Pattern INT_PATTERN = Pattern.compile("^[-+]?\\d+$");
+    private static final Pattern LONG_PATTERN = Pattern.compile("^[-+]?\\d+$\n");
+    private static final Pattern DOUBLE_PATTERN = Pattern.compile("^[-+]?\\d+(\\.\\d+)?$");
+
+
+    /**
+     *
+     * Parses the arg to an integer
+     *
+     * @param arg The arg to parse
+     * @return The potential parsed integer
+     * @deprecated Use {@link UtilParse#parseInt(String)}
+     */
+    public static Optional<Integer> parseInteger(String arg) {
+        if (arg == null) {
+            return Optional.empty();
+        }
+
+        Matcher matcher = INT_PATTERN.matcher(arg);
+
+        if (!matcher.matches()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(Integer.parseInt(arg));
+    }
+
     /**
      *
      * Parses the arg to an integer
@@ -16,12 +48,18 @@ public class UtilParse {
      * @param arg The arg to parse
      * @return The potential parsed integer
      */
-    public static Optional<Integer> parseInteger(String arg) {
-        try {
-            return Optional.of(Integer.parseInt(arg));
-        } catch (NumberFormatException e) {
-            return Optional.empty();
+    public static OptionalInt parseInt(String arg) {
+        if (arg == null) {
+            return OptionalInt.empty();
         }
+
+        Matcher matcher = INT_PATTERN.matcher(arg);
+
+        if (!matcher.matches()) {
+            return OptionalInt.empty();
+        }
+
+        return OptionalInt.of(Integer.parseInt(arg));
     }
 
     /**
@@ -31,26 +69,38 @@ public class UtilParse {
      * @param arg The arg to parse
      * @return The potential parsed long
      */
-    public static Optional<Long> parseLong(String arg) {
-        try {
-            return Optional.of(Long.parseLong(arg));
-        } catch (NumberFormatException e) {
-            return Optional.empty();
+    public static OptionalLong parseLong(String arg) {
+        if (arg == null) {
+            return OptionalLong.empty();
         }
+
+        Matcher matcher = LONG_PATTERN.matcher(arg);
+
+        if (!matcher.matches()) {
+            return OptionalLong.empty();
+        }
+
+        return OptionalLong.of(Long.parseLong(arg));
     }
 
     /**
      *
-     * Parses the arg to an double
+     * Parses the arg to a double
      *
      * @param arg The arg to parse
      * @return The potential parsed double
      */
-    public static Optional<Double> parseDouble(String arg) {
-        try {
-            return Optional.of(Double.parseDouble(arg));
-        } catch (NumberFormatException e) {
-            return Optional.empty();
+    public static OptionalDouble parseDouble(String arg) {
+        if (arg == null) {
+            return OptionalDouble.empty();
         }
+
+        Matcher matcher = DOUBLE_PATTERN.matcher(arg);
+
+        if (!matcher.matches()) {
+            return OptionalDouble.empty();
+        }
+
+        return OptionalDouble.of(Long.parseLong(arg));
     }
 }

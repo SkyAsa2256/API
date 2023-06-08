ac1b9c801fba43617ce296b4131d9a0386cc072f
Daniel Norris <dnorris443@gmail.com>
2023-06-08 16:19:36 +0100

    feat(spigot): add InventorySnapshot system

---
+++ spigot/src/main/java/com/envyful/api/spigot/player/inventory/InventorySnapshot.java
@@ -0,0 +1,33 @@
+package com.envyful.api.spigot.player.inventory;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+
+public class InventorySnapshot {
+
+    private final ItemStack[] items;
+
+    private InventorySnapshot(ItemStack[] items) {
+        this.items = items;
+    }
+
+    public static InventorySnapshot of(Player player) {
+        ItemStack[] contents = player.getInventory().getContents();
+
+        if (contents == null) {
+            return new InventorySnapshot(null);
+        }
+
+        return new InventorySnapshot(Arrays.copyOf(contents, contents.length));
+    }
+
+    public ItemStack[] getItems() {
+        return this.items;
+    }
+
+    public void restore(Player player) {
+        player.getInventory().setContents(this.items);
+    }
+}

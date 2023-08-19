bdd1a1d83efdde6581d49a82366df5d9238da5db
Daniel Norris <dnorris443@gmail.com>
2023-08-19 20:04:51 +0100

    feat(forge16/20): add UtilForgeServer#broadcast and UtilForgeServer#formattedBroadcast

--- forge16/src/main/java/com/envyful/api/forge/server/UtilForgeServer.java
+++ forge16/src/main/java/com/envyful/api/forge/server/UtilForgeServer.java
@@ -1,7 +1,14 @@
 package com.envyful.api.forge.server;
 
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.google.common.collect.Lists;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
+import java.util.Collection;
 
 /**
  *
@@ -39,5 +46,57 @@ public class UtilForgeServer {
      */
     public static void executeCommand(ServerPlayerEntity player, String command) {
         ServerLifecycleHooks.getCurrentServer().getCommands().performCommand(player.createCommandSourceStack(), command);
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void broadcast(String... messages) {
+        broadcast(Lists.newArrayList(messages));
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void broadcast(Collection<String> messages) {
+        for (String message : messages) {
+            ServerLifecycleHooks.getCurrentServer().getPlayerList().broadcastMessage(
+                    UtilChatColour.colour(message),
+                    ChatType.CHAT,
+                    Util.NIL_UUID
+            );
+        }
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void formattedBroadcast(ITextComponent... messages) {
+        formattedBroadcast(Lists.newArrayList(messages));
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void formattedBroadcast(Collection<ITextComponent> messages) {
+        for (ITextComponent message : messages) {
+            ServerLifecycleHooks.getCurrentServer().getPlayerList().broadcastMessage(
+                    message,
+                    ChatType.CHAT,
+                    Util.NIL_UUID
+            );
+        }
     }
 }
--- forge20/src/main/java/com/envyful/api/forge/server/UtilForgeServer.java
+++ forge20/src/main/java/com/envyful/api/forge/server/UtilForgeServer.java
@@ -1,7 +1,12 @@
 package com.envyful.api.forge.server;
 
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.google.common.collect.Lists;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.server.ServerLifecycleHooks;
+
+import java.util.Collection;
 
 /**
  *
@@ -34,5 +39,53 @@ public class UtilForgeServer {
      */
     public static void executeCommand(ServerPlayer player, String command) {
         ServerLifecycleHooks.getCurrentServer().getCommands().performPrefixedCommand(player.createCommandSourceStack(), command);
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void broadcast(String... messages) {
+        broadcast(Lists.newArrayList(messages));
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void broadcast(Collection<String> messages) {
+        for (String message : messages) {
+            ServerLifecycleHooks.getCurrentServer().getPlayerList().broadcastSystemMessage(
+                    UtilChatColour.colour(message), true
+            );
+        }
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void formattedBroadcast(Component... messages) {
+        formattedBroadcast(Lists.newArrayList(messages));
+    }
+
+    /**
+     *
+     * Broadcast the messages to all players online
+     *
+     * @param messages The messages
+     */
+    public static void formattedBroadcast(Collection<Component> messages) {
+        for (Component message : messages) {
+            ServerLifecycleHooks.getCurrentServer().getPlayerList().broadcastSystemMessage(
+                    message, true
+            );
+        }
     }
 }

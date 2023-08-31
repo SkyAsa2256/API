c847ce9795bdb8aa01ed227e7777fd2571f459c5
Daniel Norris <dnorris443@gmail.com>
2023-08-31 14:59:09 +0100

    ref(tests): add explanatory comments

--- commons/src/test/java/com/envyful/api/time/UtilTimeFormatTests.java
+++ commons/src/test/java/com/envyful/api/time/UtilTimeFormatTests.java
@@ -5,16 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class UtilTimeFormatTests {
 
+    // Checking the function provides correct output
     @Test
     void testTimeFormatConfig() {
         assert UtilTimeFormat.format(10_000_000L, new TimeFormatConfig()).equals("2 hours 46 minutes 40 seconds");
     }
 
+    // Checking the function provides correct output and removes non-seconds values
     @Test
     void testSmallerTimeFormatConfig() {
         assert UtilTimeFormat.format(10_000L, new TimeFormatConfig()).equals("10 seconds");
     }
 
+    // Checking the function cannot enter an infinite loop from the user entering seconds_value as a placholder
     @Test
     void testFailureForInvalidPlaceholdersSeconds() {
         assert UtilTimeFormat.format(10_000L, TimeFormatConfig.builder()
@@ -23,6 +26,7 @@ class UtilTimeFormatTests {
                 .build()).isEmpty();
     }
 
+    // Checking the function cannot enter an infinite loop from the user entering minutes_value as a placholder
     @Test
     void testFailureForInvalidPlaceholdersMinutes() {
         assert UtilTimeFormat.format(10_000L, TimeFormatConfig.builder()
@@ -31,6 +35,7 @@ class UtilTimeFormatTests {
                 .build()).isEmpty();
     }
 
+    // Checking the function cannot enter an infinite loop from the user entering hours_value as a placholder
     @Test
     void testFailureForInvalidPlaceholdersHours() {
         assert UtilTimeFormat.format(10_000L, TimeFormatConfig.builder()
@@ -39,6 +44,7 @@ class UtilTimeFormatTests {
                 .build()).isEmpty();
     }
 
+    // Checking the function cannot enter an infinite loop from the user entering days_value as a placholder
     @Test
     void testFailureForInvalidPlaceholdersDays() {
         assert UtilTimeFormat.format(10_000L, TimeFormatConfig.builder()
@@ -47,6 +53,7 @@ class UtilTimeFormatTests {
                 .build()).isEmpty();
     }
 
+    // Checking that the blank string at the end of the seconds placeholder gets trimmed (no blank text at the end of the string)
     @Test
     void testTimeFormatConfigTrimEnd() {
         assert UtilTimeFormat.format(10_000_000L, TimeFormatConfig.builder()

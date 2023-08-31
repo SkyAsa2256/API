7b27ecb087e0340ebf83ccf0e71314209f4cf44f
Daniel Norris <dnorris443@gmail.com>
2023-08-31 14:33:54 +0100

    ref(api): add safe method for UtilLogger

--- commons/src/main/java/com/envyful/api/concurrency/UtilLogger.java
+++ commons/src/main/java/com/envyful/api/concurrency/UtilLogger.java
@@ -2,6 +2,19 @@ package com.envyful.api.concurrency;
 
 import org.apache.logging.log4j.Logger;
 
+import java.util.Optional;
+
+/**
+ *
+ * Static utility class used by mods, and plugins, to prove a logging
+ * interface for the generic API classes found in this repository.
+ * <br>
+ * Best practice is to use the {@link UtilLogger#logger()} method
+ * as it is not guaranteed that every mod will provide a logger to the API
+ * and not using that could result in a {@link NullPointerException}
+ * in the code you're attempting to run
+ *
+ */
 public class UtilLogger {
 
     private static Logger logger;
@@ -10,7 +23,19 @@ public class UtilLogger {
         UtilLogger.logger = logger;
     }
 
+    /**
+     *
+     * Gets the instance of the provided logger
+     *
+     * @return The logger
+     * @deprecated Use {@link UtilLogger#logger()} to safely get the logger instance
+     */
+    @Deprecated
     public static Logger getLogger() {
         return logger;
+    }
+
+    public static Optional<Logger> logger() {
+        return Optional.ofNullable(logger);
     }
 }

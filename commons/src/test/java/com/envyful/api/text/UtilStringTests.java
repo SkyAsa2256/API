1b8492fb9de006b04f19ffcd67cf4f930b17edb4
Daniel Norris <dnorris443@gmail.com>
2023-09-01 21:23:43 +0100

    feat(api): add UtilStringTests

---
+++ commons/src/test/java/com/envyful/api/text/UtilStringTests.java
@@ -0,0 +1,66 @@
+package com.envyful.api.text;
+
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class UtilStringTests {
+
+    // Checks that shuffle is working by ensuring that the shuffled value isn't the same as the original value at least once
+    @Test
+    void shuffleStringTest() {
+        int shuffleSuccess = 0;
+        String value = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < 10; i++) {
+            if (!value.equals(UtilString.shuffle(value))) {
+                ++shuffleSuccess;
+            }
+        }
+
+        assert shuffleSuccess >= 1;
+    }
+
+    // Checks the mapping, and joining, is working correctly
+    @Test
+    void joinStringTest() {
+        List<RandomTestObject> objects = Lists.newArrayList(
+                new RandomTestObject("a"),
+                new RandomTestObject("b"),
+                new RandomTestObject("c"),
+                new RandomTestObject("d"),
+                new RandomTestObject("e")
+        );
+
+        assert UtilString.join(objects, "", RandomTestObject::toString).equalsIgnoreCase("abcde");
+    }
+
+    // Checks the mapping, joining, and delimiter is working correctly
+    @Test
+    void joinStringTestWithDelimiter() {
+        List<RandomTestObject> objects = Lists.newArrayList(
+                new RandomTestObject("a"),
+                new RandomTestObject("b"),
+                new RandomTestObject("c"),
+                new RandomTestObject("d"),
+                new RandomTestObject("e")
+        );
+
+        assert UtilString.join(objects, ",", RandomTestObject::toString).equalsIgnoreCase("a,b,c,d,e");
+    }
+
+    class RandomTestObject {
+
+        private final String value;
+
+        private RandomTestObject(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value;
+        }
+    }
+}

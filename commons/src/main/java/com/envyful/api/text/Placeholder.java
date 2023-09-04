af7ca16cb1e8d79683f19818c74a98ff7df33c8c
Daniel Norris <dnorris443@gmail.com>
2023-09-04 15:39:34 +0100

    feat(api): add Placeholder#simple and Placeholder#require functions

--- commons/src/main/java/com/envyful/api/text/Placeholder.java
+++ commons/src/main/java/com/envyful/api/text/Placeholder.java
@@ -1,6 +1,11 @@
 package com.envyful.api.text;
 
+import com.envyful.api.text.parse.SimplePlaceholder;
+import com.envyful.api.text.placeholder.OptionalPlaceholder;
+
 import javax.annotation.Nonnull;
+import java.util.function.BooleanSupplier;
+import java.util.function.UnaryOperator;
 
 /**
  *
@@ -20,4 +25,26 @@ public interface Placeholder {
     @Nonnull
     ParseResult replace(@Nonnull ParseResult line);
 
+    /**
+     *
+     * Gets a simple placeholder instance for the given function
+     *
+     * @param replacer The function handling text replacement
+     * @return The placeholder instance
+     */
+    static Placeholder simple(UnaryOperator<String> replacer) {
+        return (SimplePlaceholder) replacer::apply;
+    }
+
+    /**
+     *
+     * Creates a builder instance for an {@link OptionalPlaceholder}
+     * with the given test as the predicate
+     *
+     * @param test The required test
+     * @return The placeholder builder created
+     */
+    static OptionalPlaceholder.Builder require(BooleanSupplier test) {
+        return new OptionalPlaceholder.Builder().test(test);
+    }
 }

6e173457357d595b28aecb6f9e3b9a51317240ae
Daniel Norris <dnorris443@gmail.com>
2023-09-14 13:58:39 +0100

    fix(discord): remove Replacer usage

--- discord/src/main/java/com/envyful/api/discord/embed/UtilEmbed.java
+++ discord/src/main/java/com/envyful/api/discord/embed/UtilEmbed.java
@@ -1,8 +1,8 @@
 package com.envyful.api.discord.embed;
 
-import com.envyful.api.config.Replacer;
-import com.envyful.api.config.UtilReplacer;
 import com.envyful.api.discord.DiscordEmbed;
+import com.envyful.api.text.Placeholder;
+import com.envyful.api.text.PlaceholderFactory;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
@@ -21,37 +21,37 @@ public class UtilEmbed {
      * @param replacers Replacers for handling any configurable placeholders
      * @return The original embed
      */
-    public static MessageEmbed fromAPI(DiscordEmbed object, Replacer... replacers) {
+    public static MessageEmbed fromAPI(DiscordEmbed object, Placeholder... replacers) {
         EmbedBuilder builder = new EmbedBuilder()
-                .setTitle(UtilReplacer.getReplacedText(object.getTitle(), replacers))
+                .setTitle(PlaceholderFactory.handlePlaceholders(object.getTitle(), replacers).get(0))
                 .setColor(object.getColor());
 
         if (object.getImage() != null) {
-            builder.setImage(UtilReplacer.getReplacedText(object.getImage().getUrl(), replacers));
+            builder.setImage(PlaceholderFactory.handlePlaceholders(object.getImage().getUrl(), replacers).get(0));
         }
 
         if (object.getAuthor() != null) {
             builder.setAuthor(
-                    UtilReplacer.getReplacedText(object.getAuthor().getName(), replacers),
-                    UtilReplacer.getReplacedText(object.getAuthor().getUrl(), replacers),
-                    UtilReplacer.getReplacedText(object.getAuthor().getIconUrl(), replacers));
+                    PlaceholderFactory.handlePlaceholders(object.getAuthor().getName(), replacers).get(0),
+                    PlaceholderFactory.handlePlaceholders(object.getAuthor().getUrl(), replacers).get(0),
+                    PlaceholderFactory.handlePlaceholders(object.getAuthor().getIconUrl(), replacers).get(0));
         }
 
         if (object.getDescription() != null) {
-            builder.setDescription(UtilReplacer.getReplacedText(object.getDescription(), replacers));
+            builder.setDescription(PlaceholderFactory.handlePlaceholders(object.getDescription(), replacers).get(0));
         }
 
         if (object.getThumbnail() != null) {
-            builder.setThumbnail(UtilReplacer.getReplacedText(object.getThumbnail().getUrl(), replacers));
+            builder.setThumbnail(PlaceholderFactory.handlePlaceholders(object.getThumbnail().getUrl(), replacers).get(0));
         }
 
         if (object.getFooter() != null) {
-            builder.setFooter(UtilReplacer.getReplacedText(object.getFooter().getText(), replacers), UtilReplacer.getReplacedText(object.getFooter().getIconUrl(), replacers));
+            builder.setFooter(PlaceholderFactory.handlePlaceholders(object.getFooter().getText(), replacers).get(0), PlaceholderFactory.handlePlaceholders(object.getFooter().getIconUrl(), replacers).get(0));
         }
 
         for (DiscordEmbed.Field field : object.getFields()) {
-            builder.addField(UtilReplacer.getReplacedText(field.getName(), replacers),
-                    UtilReplacer.getReplacedText(field.getValue(), replacers),
+            builder.addField(PlaceholderFactory.handlePlaceholders(field.getName(), replacers).get(0),
+                    PlaceholderFactory.handlePlaceholders(field.getValue(), replacers).get(0),
                     field.isInline());
         }
 

98b254caf524106faa18bfec9b54b6e7c50f897d
Daniel Norris <dnorris443@gmail.com>
2023-09-14 09:26:30 +0100

    ref(api): change TabCompletions annotation to be named CompletionHandler inline with other handler annotations

--- commons/src/main/java/com/envyful/api/command/annotate/executor/CompletionHandler.java
+++ commons/src/main/java/com/envyful/api/command/annotate/executor/CompletionHandler.java
@@ -1,4 +1,4 @@
-package com.envyful.api.command.annotate;
+package com.envyful.api.command.annotate.executor;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface TabCompletions {
+public @interface CompletionHandler {
 }

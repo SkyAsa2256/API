88c1b7437c3fe6064a5fc416d700b877f3c0f289
Daniel Norris <dnorris443@gmail.com>
2023-09-14 14:57:21 +0100

    fix(forge20): automatically register Permissible permissions

--- forge20/src/main/java/com/envyful/api/forge/command/parser/ForgeAnnotationCommandParser.java
+++ forge20/src/main/java/com/envyful/api/forge/command/parser/ForgeAnnotationCommandParser.java
@@ -2,12 +2,55 @@ package com.envyful.api.forge.command.parser;
 
 import com.envyful.api.command.CommandFactory;
 import com.envyful.api.command.annotate.AnnotationCommandParser;
+import com.envyful.api.command.annotate.permission.Permissible;
+import com.envyful.api.command.annotate.permission.PermissionHandler;
+import com.envyful.api.command.exception.CommandParseException;
+import com.envyful.api.concurrency.UtilLogger;
 import com.envyful.api.forge.command.command.ForgePlatformCommand;
+import com.envyful.api.forge.player.util.UtilPlayer;
+import com.envyful.api.type.BooleanBiFunction;
 import net.minecraft.commands.CommandSource;
+
+import java.lang.reflect.Method;
+import java.util.List;
 
 public class ForgeAnnotationCommandParser extends AnnotationCommandParser<ForgePlatformCommand, CommandSource> {
 
     public ForgeAnnotationCommandParser(CommandFactory<?, CommandSource> commandFactory) {
         super(commandFactory, CommandSource.class);
+    }
+
+    @Override
+    protected BooleanBiFunction<CommandSource, List<String>> getPermissionCheck(Object o) {
+        Permissible permissible = o.getClass().getAnnotation(Permissible.class);
+
+        if (permissible != null) {
+            UtilPlayer.registerPermission(permissible.value());
+            return (sender, args) -> this.commandFactory.hasPermission(sender, permissible.value());
+        }
+
+        for (Method declaredMethod : o.getClass().getDeclaredMethods()) {
+            PermissionHandler annotation = declaredMethod.getAnnotation(PermissionHandler.class);
+
+            if (annotation == null) {
+                continue;
+            }
+
+            if (!this.isPermissionHandlerMethod(declaredMethod)) {
+                throw new CommandParseException("Method " + declaredMethod.getName() + " in class " + o.getClass().getName() + " is not a valid permission handler method");
+            }
+
+            return (sender, args) -> {
+                try {
+                    return (boolean) declaredMethod.invoke(o, sender, args);
+                } catch (Exception e) {
+                    UtilLogger.logger().ifPresent(logger -> logger.error("Error occurred when performing permission check for command " + o.getClass().getSimpleName(), e));
+                }
+
+                return false;
+            };
+        }
+
+        return null;
     }
 }

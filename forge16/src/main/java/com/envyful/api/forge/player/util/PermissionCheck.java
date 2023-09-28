55d6fbd8e2cb2719ee015b883912b489d07385a0
Daniel Norris <dnorris443@gmail.com>
2023-09-28 13:57:45 +0100

    feat(forge16/20): add PermissionCheck

---
+++ forge16/src/main/java/com/envyful/api/forge/player/util/PermissionCheck.java
@@ -0,0 +1,48 @@
+package com.envyful.api.forge.player.util;
+
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.google.common.collect.Lists;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.util.List;
+
+/**
+ *
+ * Simple utility class for checking if a player has a permission node
+ * and if not sending them the given message
+ *
+ */
+@ConfigSerializable
+public class PermissionCheck {
+
+    protected boolean enabled;
+    protected String permission;
+    protected List<String> noPermissionMessage;
+
+    public PermissionCheck() {
+    }
+
+    protected PermissionCheck(boolean enabled, String permission, List<String> noPermissionMessage) {
+        this.enabled = enabled;
+        this.permission = permission;
+        this.noPermissionMessage = noPermissionMessage;
+    }
+
+    public boolean test(ForgeEnvyPlayer player) {
+        return this.test(player.getParent());
+    }
+
+    public boolean test(ServerPlayerEntity player) {
+        if (!this.enabled || UtilPlayer.hasPermission(player, this.permission)) {
+            return true;
+        }
+
+        UtilPlayer.sendMessage(player, this.noPermissionMessage);
+        return false;
+    }
+
+    public static PermissionCheck of(String permission, String... failureMessage) {
+        return new PermissionCheck(true, permission, Lists.newArrayList(failureMessage));
+    }
+}

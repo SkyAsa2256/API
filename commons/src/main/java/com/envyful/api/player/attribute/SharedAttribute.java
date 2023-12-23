2e0b5fc1594e3b195b6d3aa6abcd9b746733e49a
Daniel Norris <dnorris443@gmail.com>
2023-12-23 12:06:06 +0000

    feat(api): add default implementation for Attribute#deleteAll to SharedAttribute

--- commons/src/main/java/com/envyful/api/player/attribute/SharedAttribute.java
+++ commons/src/main/java/com/envyful/api/player/attribute/SharedAttribute.java
@@ -1,7 +1,17 @@
 package com.envyful.api.player.attribute;
 
+import com.envyful.api.concurrency.UtilLogger;
+import com.envyful.api.database.Database;
 import com.envyful.api.player.PlayerManager;
+import com.envyful.api.player.attribute.data.TableData;
+import com.envyful.api.player.save.SaveManager;
+import com.envyful.api.player.save.attribute.DataDirectory;
+import com.envyful.api.player.save.impl.JsonSaveManager;
+import com.envyful.api.player.save.impl.SQLSaveManager;
+import com.google.common.collect.Lists;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +29,7 @@ public abstract class SharedAttribute<A, B> extends ManagedAttribute<A, B> {
 
     protected A id;
     protected long lastSave = -1L;
+    protected Database database;
 
     protected SharedAttribute(B manager, PlayerManager<?, ?> playerManager) {
         super(manager);
@@ -63,5 +74,50 @@ public abstract class SharedAttribute<A, B> extends ManagedAttribute<A, B> {
     @Override
     public CompletableFuture<A> getId() {
         return CompletableFuture.completedFuture(this.id);
+    }
+
+    @Override
+    public void deleteAll() {
+        this.findTargets().forEach(s -> {
+            if (this.getDatabase() == null) {
+                this.playerManager.getSaveManager().delete(s);
+            } else {
+                this.playerManager.getSaveManager().delete(this.getDatabase(), s);
+            }
+        });
+    }
+
+    protected Database getDatabase() {
+        return this.database;
+    }
+
+    protected List<String> findTargets() {
+        List<String> targets = Lists.newArrayList();
+        SaveManager<?> saveManager = this.playerManager.getSaveManager();
+
+        if (saveManager instanceof SQLSaveManager) {
+            TableData tableData = this.getClass().getAnnotation(TableData.class);
+
+            if (tableData == null) {
+                UtilLogger.logger().ifPresent(logger -> logger.error("No table data annotation found for {}", this.getClass().getName()));
+                return Collections.emptyList();
+            }
+
+            targets.addAll(Lists.newArrayList(tableData.value()));
+        } else if (saveManager instanceof JsonSaveManager) {
+            DataDirectory dataDirectory = this.getClass().getAnnotation(DataDirectory.class);
+
+            if (dataDirectory == null) {
+                UtilLogger.logger().ifPresent(logger -> logger.error("No data directory annotation found for {}", this.getClass().getName()));
+                return Collections.emptyList();
+            }
+
+            targets.addAll(Lists.newArrayList(dataDirectory.value()));
+        } else {
+            UtilLogger.logger().ifPresent(logger -> logger.error("Unknown save manager type {}", saveManager.getClass().getName()));
+            return Collections.emptyList();
+        }
+
+        return targets;
     }
 }

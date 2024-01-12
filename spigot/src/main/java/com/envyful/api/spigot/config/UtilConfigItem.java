08b8858cc81445f4a75297a76bd5c9573cea0262
Daniel Norris <dnorris443@gmail.com>
2024-01-12 09:48:02 +0000

    fix(spigot): apply placeholders to type

--- spigot/src/main/java/com/envyful/api/spigot/config/UtilConfigItem.java
+++ spigot/src/main/java/com/envyful/api/spigot/config/UtilConfigItem.java
@@ -81,7 +81,7 @@ public class UtilConfigItem {
         String name = configItem.getName();
 
         ItemBuilder itemBuilder = new ItemBuilder()
-                .type(Material.valueOf(configItem.getType()))
+                .type(Material.valueOf(PlaceholderFactory.handlePlaceholders(configItem.getType(),placeholders).get(0)))
                 .amount(configItem.getAmount(placeholders));
 
         itemBuilder.lore(PlaceholderFactory.handlePlaceholders(configItem.getLore(), input -> MiniMessage.miniMessage().deserialize(input).decoration(TextDecoration.ITALIC, false), placeholders));

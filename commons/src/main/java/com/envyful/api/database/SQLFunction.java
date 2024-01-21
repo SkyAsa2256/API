bbf8000c679d724416f423751a4972d164a88f3e
Daniel Norris <dnorris443@gmail.com>
2024-01-21 13:46:15 +0000

    ref(api): add javadocs for SQLFunction

--- commons/src/main/java/com/envyful/api/database/SQLFunction.java
+++ commons/src/main/java/com/envyful/api/database/SQLFunction.java
@@ -2,8 +2,25 @@ package com.envyful.api.database;
 
 import java.sql.SQLException;
 
+/**
+ *
+ * Represents a function that takes in a value and returns a value
+ * and can throw an SQLException
+ *
+ * @param <A> The input type
+ * @param <B> The output type
+ */
+@FunctionalInterface
 public interface SQLFunction<A, B> {
 
+    /**
+     *
+     * Applies the function
+     *
+     * @param a The input
+     * @return The output
+     * @throws SQLException An error if there's an issue
+     */
     B apply(A a) throws SQLException;
 
 }

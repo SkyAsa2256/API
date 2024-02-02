75c88581eed08a72018a1b7b8f95ffd9cf7338e5
Daniel Norris <dnorris443@gmail.com>
2024-02-02 15:11:13 +0000

    feat(jexl): add CalculationConfig

---
+++ jexl/src/main/java/com/envyful/api/jexl/config/CalculationConfig.java
@@ -0,0 +1,32 @@
+package com.envyful.api.jexl.config;
+
+import com.envyful.api.jexl.UtilJexl;
+import org.apache.commons.jexl3.JexlExpression;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigSerializable
+public class CalculationConfig {
+
+    private String calculation;
+    private transient JexlExpression expression;
+
+    public CalculationConfig() {
+    }
+
+    public CalculationConfig(String calculation) {
+        this.calculation = calculation;
+    }
+
+    public JexlExpression getExpression() {
+        if (this.calculation == null || this.calculation.isEmpty()) {
+            return null;
+        }
+
+        if (this.expression == null) {
+            this.expression = UtilJexl.getEngine().createExpression(this.calculation);
+        }
+
+        return this.expression;
+    }
+
+}

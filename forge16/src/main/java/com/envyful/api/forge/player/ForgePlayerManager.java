b3ffd07044783136ae8a976233f1246241d5fbc6
Daniel Norris <dnorris443@gmail.com>
2024-02-13 23:08:13 +0000

    feat(forge16): add ForgeTrigger and update ForgePlayerManager

--- forge16/src/main/java/com/envyful/api/forge/player/ForgePlayerManager.java
+++ forge16/src/main/java/com/envyful/api/forge/player/ForgePlayerManager.java
@@ -1,14 +1,13 @@
 package com.envyful.api.forge.player;
 
 import com.envyful.api.concurrency.UtilConcurrency;
-import com.envyful.api.concurrency.UtilLogger;
+import com.envyful.api.forge.concurrency.UtilForgeConcurrency;
+import com.envyful.api.forge.player.attribute.ForgeTrigger;
+import com.envyful.api.player.Attribute;
+import com.envyful.api.player.AttributeBuilder;
 import com.envyful.api.player.PlayerManager;
-import com.envyful.api.player.attribute.Attribute;
-import com.envyful.api.player.attribute.data.PlayerAttributeData;
-import com.envyful.api.player.save.SaveManager;
-import com.envyful.api.player.save.impl.EmptySaveManager;
+import com.envyful.api.player.manager.AbstractPlayerManager;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -16,12 +15,6 @@ import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
-
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 
 /**
  *
@@ -31,195 +24,48 @@ import java.util.function.Supplier;
  *
  * Simple instantiation as not enough arguments to warrant a builder class and
  */
-public class ForgePlayerManager implements PlayerManager<ForgeEnvyPlayer, ServerPlayerEntity> {
-
-    private final Map<UUID, ForgeEnvyPlayer> cachedPlayers = Maps.newHashMap();
-    private final List<PlayerAttributeData> attributeData = Lists.newArrayList();
-
-    private SaveManager<ServerPlayerEntity> saveManager = new EmptySaveManager<>(this);
+public class ForgePlayerManager extends AbstractPlayerManager<ForgeEnvyPlayer, ServerPlayerEntity> {
 
     public ForgePlayerManager() {
-        MinecraftForge.EVENT_BUS.register(new PlayerListener(this));
+        super(ServerPlayerEntity::getUUID);
+
+        MinecraftForge.EVENT_BUS.register(new PlayerListener());
         UsernameFactory.init();
     }
 
-    @Override
-    public ForgeEnvyPlayer getPlayer(ServerPlayerEntity player) {
-        return this.getPlayer(player.getUUID());
-    }
 
     @Override
-    public ForgeEnvyPlayer getPlayer(UUID uuid) {
-        return this.cachedPlayers.get(uuid);
+    public <X extends Attribute<Y, ServerPlayerEntity>, Y> void registerAttribute(AttributeBuilder<X, Y, ServerPlayerEntity> builder) {
+        builder.triggers(
+                ForgeTrigger.singleSet(PlayerEvent.PlayerLoggedInEvent.class, event -> this.cachedPlayers.get(event.getEntity().getUUID())),
+                ForgeTrigger.singleSave(PlayerEvent.PlayerLoggedOutEvent.class, event -> this.cachedPlayers.get(event.getEntity().getUUID())),
+                ForgeTrigger.save(WorldEvent.Save.class, event -> Lists.newArrayList(this.cachedPlayers.values())),
+                ForgeTrigger.save(FMLServerStoppingEvent.class, event -> Lists.newArrayList(this.cachedPlayers.values()))
+        );
+
+        super.registerAttribute(builder);
     }
 
-    @Override
-    public ForgeEnvyPlayer getOnlinePlayer(String username) {
-        for (ForgeEnvyPlayer online : this.cachedPlayers.values()) {
-            if (online.getParent().getName().getString().equals(username)) {
-                return online;
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public ForgeEnvyPlayer getOnlinePlayerCaseInsensitive(String username) {
-        for (ForgeEnvyPlayer online : this.cachedPlayers.values()) {
-            if (online.getParent().getName().getString().equalsIgnoreCase(username)) {
-                return online;
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public List<ForgeEnvyPlayer> getOnlinePlayers() {
-        return Collections.unmodifiableList(Lists.newArrayList(this.cachedPlayers.values()));
-    }
-
-    @Override
-    public List<Attribute<?>> getOfflineAttributes(UUID uuid) {
-        try {
-            return this.saveManager.loadData(uuid).get();
-        } catch (InterruptedException | ExecutionException e) {
-            return Collections.emptyList();
-        }
-    }
-
-    @Override
-    public <A extends Attribute<B>, B> void registerAttribute(Class<A> attribute, Supplier<A> constructor) {
-        this.attributeData.add(new PlayerAttributeData(attribute));
-
-        if (this.saveManager != null) {
-            this.saveManager.registerAttribute(attribute, constructor);
-        }
-    }
-
-    @Override
-    public void setSaveManager(SaveManager<ServerPlayerEntity> saveManager) {
-        this.saveManager = saveManager;
-    }
-
-    @Override
-    public SaveManager<ServerPlayerEntity> getSaveManager() {
-        return this.saveManager;
-    }
-
-    @Override
-    public <A extends Attribute<B>, B> CompletableFuture<A> loadAttribute(Class<? extends A> attributeClass, B id) {
-        return this.saveManager.loadAttribute(attributeClass, id);
-    }
-
-    private static final class PlayerListener {
-
-        private final ForgePlayerManager manager;
-        private long lastSave = -1L;
-
-        private PlayerListener(ForgePlayerManager manager) {
-            this.manager = manager;
-        }
+    private final class PlayerListener {
 
         @SubscribeEvent(priority = EventPriority.LOWEST)
         public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
-            ForgeEnvyPlayer player = new ForgeEnvyPlayer(this.manager.saveManager,
-                    (ServerPlayerEntity) event.getPlayer());
-            this.manager.cachedPlayers.put(event.getPlayer().getUUID(), player);
-
-            UtilConcurrency.runAsync(() -> {
-                this.manager.saveManager.loadData(player).whenComplete((attributes, throwable) -> {
-                    if (throwable != null) {
-                        this.manager.saveManager.getErrorHandler().accept(player, throwable);
-                        return;
-                    }
-
-                    for (PlayerAttributeData attributeDatum : this.manager.attributeData) {
-                        Attribute<?> attribute = this.findAttribute(attributeDatum, attributes);
-
-                        if (attribute == null) {
-                            UtilLogger.logger().ifPresent(logger -> logger.error("Null attribute loaded for {}", attributeDatum.getAttributeClass().getName()));
-                            continue;
-                        }
-
-                        player.setAttribute(attribute);
-                    }
-                });
-            });
-        }
-
-        private Attribute<?> findAttribute(PlayerAttributeData attributeDatum,
-                                                 List<Attribute<?>> playerAttributes) {
-            for (Attribute<?> playerAttribute : playerAttributes) {
-                if (Objects.equals(attributeDatum.getAttributeClass(), playerAttribute.getClass())) {
-                    return playerAttribute;
-                }
-            }
-
-            return null;
+            var player = new ForgeEnvyPlayer(saveManager, (ServerPlayerEntity) event.getPlayer());
+            cachedPlayers.put(event.getPlayer().getUUID(), player);
         }
 
         @SubscribeEvent(priority = EventPriority.LOWEST)
         public void onPlayerQuit(PlayerEvent.PlayerLoggedOutEvent event) {
-            ForgeEnvyPlayer player = this.manager.cachedPlayers.remove(event.getPlayer().getUUID());
-
-            if (player == null) {
-                return;
-            }
-
-            UtilConcurrency.runAsync(() -> {
-                for (Attribute<?> value : player.getAttributes()) {
-                    if (value != null) {
-                        this.manager.saveManager.saveData(player, value);
-                    }
-                }
-            });
-        }
-
-        @SubscribeEvent
-        public void onWorldSave(WorldEvent.Save event) {
-            if (!this.shouldSave()) {
-                return;
-            }
-
-            this.lastSave = System.currentTimeMillis();
-
-            UtilConcurrency.runAsync(() -> {
-                for (ForgeEnvyPlayer onlinePlayer : this.manager.getOnlinePlayers()) {
-                    for (Attribute<?> value : onlinePlayer.getAttributes()) {
-                        if (value != null) {
-                            this.manager.saveManager.saveData(onlinePlayer, value);
-                        }
-                    }
-                }
-            });
-        }
-
-        private boolean shouldSave() {
-            return this.lastSave == -1 || (System.currentTimeMillis() - this.lastSave) >= TimeUnit.MINUTES.toMillis(2);
+            UtilForgeConcurrency.runLater(() -> cachedPlayers.remove(event.getEntity().getUUID()), 40);
         }
 
         @SubscribeEvent(priority = EventPriority.LOWEST)
         public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
             UtilConcurrency.runLater(() -> {
-                ForgeEnvyPlayer player = this.manager.cachedPlayers.get(event.getPlayer().getUUID());
+                ForgeEnvyPlayer player = cachedPlayers.get(event.getPlayer().getUUID());
 
                 player.setParent((ServerPlayerEntity) event.getPlayer());
             }, 5L);
-        }
-
-        @SubscribeEvent(priority = EventPriority.HIGHEST)
-        public void onPreServerShutdown(FMLServerStoppingEvent event) {
-            UtilConcurrency.runAsync(() -> {
-                for (ForgeEnvyPlayer player : this.manager.cachedPlayers.values()) {
-                    for (Attribute<?> value : player.getAttributes()) {
-                        if (value != null) {
-                            this.manager.saveManager.saveData(player, value);
-                        }
-                    }
-                }
-            });
         }
     }
 }
---
+++ forge16/src/main/java/com/envyful/api/forge/player/attribute/ForgeTrigger.java
@@ -0,0 +1,214 @@
+package com.envyful.api.forge.player.attribute;
+
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.api.player.EnvyPlayer;
+import com.envyful.api.player.attribute.AttributeTrigger;
+import com.envyful.api.player.attribute.trigger.ClearAttributeTrigger;
+import com.envyful.api.player.attribute.trigger.SaveAttributeTrigger;
+import com.envyful.api.player.attribute.trigger.SetAttributeTrigger;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ *
+ * Static helper class for creating triggers for Forge
+ *
+ */
+public class ForgeTrigger {
+
+    private ForgeTrigger() {
+        throw new UnsupportedOperationException("Cannot instantiate a static class");
+    }
+
+
+    /**
+     *
+     * Creates a trigger to set the attribute for a single player
+     *
+     * @param event the event
+     * @param converter the converter to convert the event to a player
+     * @return the trigger
+     * @param <A> the event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> singleSet(Class<A> event, Function<A, ForgeEnvyPlayer> converter) {
+        return singleSet(MinecraftForge.EVENT_BUS, event, converter);
+    }
+
+    /**
+     *
+     * Creates a trigger to set the attribute for a single player
+     *
+     * @param eventBus The event bus
+     * @param event The event
+     * @param converter The converter to convert the event to a player
+     * @return The trigger
+     * @param <A> The event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> singleSet(IEventBus eventBus, Class<A> event, Function<A, ForgeEnvyPlayer> converter) {
+        return set(eventBus, event, a -> List.of(converter.apply(a)));
+    }
+
+    /**
+     *
+     * Creates a trigger to set the attribute for multiple players
+     *
+     * @param event the event
+     * @param converter the converter to convert the event to a list of players
+     * @return the trigger
+     * @param <A> the event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> set(Class<A> event, Function<A, List<ForgeEnvyPlayer>> converter) {
+        return set(MinecraftForge.EVENT_BUS, event, converter);
+    }
+
+    /**
+     *
+     * Creates a trigger to set the attribute for multiple players
+     *
+     * @param eventBus The event bus
+     * @param event The event
+     * @param converter The converter to convert the event to a list of players
+     * @return The trigger
+     * @param <A> The event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> set(IEventBus eventBus, Class<A> event, Function<A, List<ForgeEnvyPlayer>> converter) {
+        var trigger = new SetAttributeTrigger<ServerPlayerEntity>();
+        createHandler(eventBus, event, converter, trigger::trigger);
+        return trigger;
+    }
+
+    /**
+     *
+     * Creates a trigger to clear the attribute for a single player
+     *
+     * @param event the event
+     * @param converter the converter to convert the event to a player
+     * @return the trigger
+     * @param <A> the event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> singleClear(Class<A> event, Function<A, ForgeEnvyPlayer> converter) {
+        return singleClear(MinecraftForge.EVENT_BUS, event, converter);
+    }
+
+    /**
+     *
+     * Creates a trigger to clear the attribute for a single player
+     *
+     * @param eventBus The event bus
+     * @param event The event
+     * @param converter The converter to convert the event to a player
+     * @return The trigger
+     * @param <A> The event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> singleClear(IEventBus eventBus, Class<A> event, Function<A, ForgeEnvyPlayer> converter) {
+        return clear(eventBus, event, a -> List.of(converter.apply(a)));
+    }
+
+    /**
+     *
+     * Creates a trigger to clear the attribute for multiple players
+     *
+     * @param event the event
+     * @param converter the converter to convert the event to a list of players
+     * @return the trigger
+     * @param <A> the event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> clear(Class<A> event, Function<A, List<ForgeEnvyPlayer>> converter) {
+        return clear(MinecraftForge.EVENT_BUS, event, converter);
+    }
+
+    /**
+     *
+     * Creates a trigger to clear the attribute for multiple players
+     *
+     * @param eventBus The event bus
+     * @param event The event
+     * @param converter The converter to convert the event to a list of players
+     * @return The trigger
+     * @param <A> The event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> clear(IEventBus eventBus, Class<A> event, Function<A, List<ForgeEnvyPlayer>> converter) {
+        var trigger = new ClearAttributeTrigger<ServerPlayerEntity>();
+        createHandler(eventBus, event, converter, trigger::trigger);
+        return trigger;
+    }
+
+    /**
+     *
+     * Creates a trigger to save the attribute data for a single player
+     *
+     * @param event the event
+     * @param converter the converter to convert the event to a player
+     * @return the trigger
+     * @param <A> the event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> singleSave(Class<A> event, Function<A, ForgeEnvyPlayer> converter) {
+        return save(MinecraftForge.EVENT_BUS, event, a -> List.of(converter.apply(a)));
+    }
+
+
+    /**
+     *
+     * Creates a trigger to save the attribute data for a single player
+     *
+     * @param eventBus The event bus
+     * @param event The event
+     * @param converter The converter to convert the event to a player
+     * @return The trigger
+     * @param <A> The event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> singleSave(IEventBus eventBus, Class<A> event, Function<A, ForgeEnvyPlayer> converter) {
+        return save(eventBus, event, a -> List.of(converter.apply(a)));
+    }
+
+    /**
+     *
+     * Creates a trigger to save the attribute data for multiple players
+     *
+     * @param event the event
+     * @param converter the converter to convert the event to a list of players
+     * @return the trigger
+     * @param <A> the event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> save(Class<A> event, Function<A, List<ForgeEnvyPlayer>> converter) {
+        return save(MinecraftForge.EVENT_BUS, event, converter);
+    }
+
+
+    /**
+     *
+     * Creates a trigger to save the attribute data for multiple players
+     *
+     * @param eventBus The event bus
+     * @param event The event
+     * @param converter The converter to convert the event to a list of players
+     * @return The trigger
+     * @param <A> The event type
+     */
+    public static <A extends Event> AttributeTrigger<ServerPlayerEntity> save(IEventBus eventBus, Class<A> event, Function<A, List<ForgeEnvyPlayer>> converter) {
+        var trigger = new SaveAttributeTrigger<ServerPlayerEntity>();
+        createHandler(eventBus, event, converter, trigger::trigger);
+        return trigger;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <A extends Event> void createHandler(IEventBus eventBus, Class<A> eventClass, Function<A, List<ForgeEnvyPlayer>> converter, Consumer<EnvyPlayer<ServerPlayerEntity>> trigger) {
+        eventBus.addListener(event -> {
+            if (!eventClass.isAssignableFrom(event.getClass())) {
+                return;
+            }
+
+            for (var player : converter.apply((A) event)) {
+                if (player != null) {
+                    trigger.accept(player);
+                }
+            }
+        });
+    }
+}

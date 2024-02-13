8b7bc2226f0d81b13a0b6d94131db88ff63c0e1b
Daniel Norris <dnorris443@gmail.com>
2024-02-13 23:12:05 +0000

    fix(api): update javadocs for PlayerManager

--- commons/src/main/java/com/envyful/api/player/PlayerManager.java
+++ commons/src/main/java/com/envyful/api/player/PlayerManager.java
@@ -113,7 +113,7 @@ public interface PlayerManager<A extends EnvyPlayer<B>, B> {
       * instantiated it can be created (using reflection) from the registry in the PlayerManager implementation.
       * <br>
       * If {@link PlayerManager#setSaveManager(SaveManager)} has been called then it will call
-      * {@link SaveManager#registerAttribute(Class, Supplier)} on the given class
+      * {@link SaveManager#registerAttribute(AttributeData)} on the given class
       *
       * @param builder The builder for the attribute
       * @param <X> The attribute type
@@ -143,7 +143,7 @@ public interface PlayerManager<A extends EnvyPlayer<B>, B> {
       * instantiated it can be created (using reflection) from the registry in the PlayerManager implementation.
       * <br>
       * If {@link PlayerManager#setSaveManager(SaveManager)} has been called then it will call
-      * {@link SaveManager#registerAttribute(Class, Supplier)} on the given class
+      * {@link SaveManager#registerAttribute(AttributeData)}  on the given class
       *
       * @param attributeData The data for the attribute
       * @param <X> The attribute type

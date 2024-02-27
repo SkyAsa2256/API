4a15ff2a83733069dff88147480170743812af60
Daniel Norris <dnorris443@gmail.com>
2024-02-27 13:04:54 +0000

    feat(api): javadoc for PlatformCommandExecutor

--- commons/src/main/java/com/envyful/api/command/PlatformCommandExecutor.java
+++ commons/src/main/java/com/envyful/api/command/PlatformCommandExecutor.java
@@ -1,7 +1,20 @@
 package com.envyful.api.command;
 
+/**
+ *
+ * An interface that represents a command executor for a specific platform
+ *
+ * @param <C> The sender type for the platform specific implementation
+ */
 public interface PlatformCommandExecutor<C> {
 
+    /**
+     *
+     * The method for executing the command
+     *
+     * @param sender The sender of the command
+     * @param args The arguments for the command
+     */
     void execute(C sender, String[] args);
 
 }

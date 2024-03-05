78c38b044209f919b1548f216d5392a374011cd7
Daniel Norris <dnorris443@gmail.com>
2024-03-05 15:15:21 +0000

    ref(api): add javadocs for TabHandler and SimpleTabHandler

--- commons/src/main/java/com/envyful/api/command/tab/SimpleTabHandler.java
+++ commons/src/main/java/com/envyful/api/command/tab/SimpleTabHandler.java
@@ -3,9 +3,24 @@ package com.envyful.api.command.tab;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ *
+ * An interface for handling tab completions for commands
+ * but covering the case where no async operations are needed
+ *
+ * @param <A> The sender type
+ */
 @FunctionalInterface
 public interface SimpleTabHandler<A> extends TabHandler<A> {
 
+    /**
+     *
+     * The method to complete the tab
+     *
+     * @param sender The sender
+     * @param args The arguments
+     * @return The list of completions
+     */
     List<String> complete(A sender, String[] args);
 
     @Override
--- commons/src/main/java/com/envyful/api/command/tab/TabHandler.java
+++ commons/src/main/java/com/envyful/api/command/tab/TabHandler.java
@@ -3,9 +3,23 @@ package com.envyful.api.command.tab;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ *
+ * Interface for handling tab completions for commands
+ *
+ * @param <A> The sender type
+ */
 @FunctionalInterface
 public interface TabHandler<A> {
 
+    /**
+     *
+     * Gets the completions for the sender and args
+     *
+     * @param sender The sender
+     * @param args The arguments
+     * @return The future of the completions
+     */
     CompletableFuture<List<String>> getCompletions(A sender, String[] args);
 
 }

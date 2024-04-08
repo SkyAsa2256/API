36eb060be0d1068ed2a55a41cc1edc5472a0d3a2
Daniel Norris <dnorris443@gmail.com>
2024-04-08 23:13:55 +0100

    doc(api): javadoc config annotations

--- commons/src/main/java/com/envyful/api/config/data/ScalarSerializers.java
+++ commons/src/main/java/com/envyful/api/config/data/ScalarSerializers.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ *
+ * An annotation to specify the scalar serializers for a config object
+ *
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ScalarSerializers {
--- commons/src/main/java/com/envyful/api/config/data/TypeSerializers.java
+++ commons/src/main/java/com/envyful/api/config/data/TypeSerializers.java
@@ -1,6 +1,5 @@
 package com.envyful.api.config.data;
 
-import org.spongepowered.configurate.serialize.ScalarSerializer;
 import org.spongepowered.configurate.serialize.TypeSerializer;
 
 import java.lang.annotation.ElementType;
@@ -8,6 +7,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *
+ * An annotation to specify the type serializers for a config object
+ * <br>
+ * The {@link #serializer()} and {@link #clazz()} arrays must be the same length and
+ * the index of the serializer must correspond to the index of the class
+ *
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TypeSerializers {

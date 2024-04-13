5dddb9a7219cc2f0ac2b419d781da6b6d7c4045d
Daniel Norris <dnorris443@gmail.com>
2024-04-13 22:50:48 +0100

    ref(api): add javadocs for UtilTime

--- commons/src/main/java/com/envyful/api/time/UtilTime.java
+++ commons/src/main/java/com/envyful/api/time/UtilTime.java
@@ -7,10 +7,22 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *
+ * Static utility class for handling time related operations
+ *
+ */
 public class UtilTime {
 
     private static final Pattern timeString = Pattern.compile("(((\\d+)w)?((\\d+)d)?((\\d+)h)?((\\d+)m)?((\\d+)s)?)");
 
+    /**
+     *
+     * Attempts to parse a time duration in milliseconds from the provided string
+     *
+     * @param entry The string to parse
+     * @return The time in milliseconds
+     */
     public static Optional<Long> attemptParseTime(String entry) {
         Matcher matcher = timeString.matcher(entry);
 
@@ -37,10 +49,23 @@ public class UtilTime {
         }
     }
 
+    /**
+     *
+     * Gets the time in milliseconds at the start of the current day
+     *
+     * @return The time in milliseconds
+     */
     public static long getStartOfDay() {
         return getStartOfDay(TimeUnit.MILLISECONDS);
     }
 
+    /**
+     *
+     * Gets the time in the specified time unit at the start of the current day
+     *
+     * @param timeUnit The time unit to return the time in
+     * @return The time in the specified time unit
+     */
     public static long getStartOfDay(TimeUnit timeUnit) {
         var currentDate = LocalDate.now();
         var startOfDay = currentDate.atStartOfDay();

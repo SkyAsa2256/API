14ebfab96fdb9c5b3283620c6f1d0894ffe45904
Daniel Norris <dnorris443@gmail.com>
2024-04-20 19:15:21 +0100

    ref(api): add javadocs for UtilJexl.java

--- jexl/src/main/java/com/envyful/api/jexl/UtilJexl.java
+++ jexl/src/main/java/com/envyful/api/jexl/UtilJexl.java
@@ -3,6 +3,11 @@ package com.envyful.api.jexl;
 import org.apache.commons.jexl3.JexlBuilder;
 import org.apache.commons.jexl3.JexlEngine;
 
+/**
+ *
+ * Static utility class for Jexl storing an instance of the JexlEngine
+ *
+ */
 public class UtilJexl {
 
     private static final JexlEngine ENGINE = new JexlBuilder().create();

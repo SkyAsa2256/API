d7783eb2cef8d42be42589da5b2a226b00bb14be
Daniel Norris <dnorris443@gmail.com>
2024-05-20 20:37:45 +0100

    ref(velocity): javadoc

--- velocity/src/main/java/com/envyful/api/velocity/player/attribute/ManagedVelocityAttribute.java
+++ velocity/src/main/java/com/envyful/api/velocity/player/attribute/ManagedVelocityAttribute.java
@@ -4,6 +4,12 @@ import com.envyful.api.player.attribute.PlayerAttribute;
 import com.envyful.api.velocity.player.VelocityEnvyPlayer;
 import com.velocitypowered.api.proxy.Player;
 
+/**
+ *
+ * An abstract class for a managed attribute for a {@link VelocityEnvyPlayer}
+ *
+ * @param <A> The type of the attribute
+ */
 public abstract class ManagedVelocityAttribute<A>
         extends PlayerAttribute<A, VelocityEnvyPlayer, Player> {
 

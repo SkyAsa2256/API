c1d8b8670f2d09a4d17ac49e9078783743a5fec0
Daniel Norris <dnorris443@gmail.com>
2024-05-27 19:08:38 +0100

    ref(velocity): javadoc VelocityAnnotationCommandParser.java

--- velocity/src/main/java/com/envyful/api/velocity/player/command/parser/VelocityAnnotationCommandParser.java
+++ velocity/src/main/java/com/envyful/api/velocity/player/command/parser/VelocityAnnotationCommandParser.java
@@ -5,6 +5,11 @@ import com.envyful.api.command.annotate.AnnotationCommandParser;
 import com.envyful.api.velocity.player.command.command.VelocityPlatformCommand;
 import com.velocitypowered.api.command.CommandSource;
 
+/**
+ *
+ * Velocity implementation of the generic {@link AnnotationCommandParser} class
+ *
+ */
 public class VelocityAnnotationCommandParser extends AnnotationCommandParser<VelocityPlatformCommand, CommandSource> {
 
     public VelocityAnnotationCommandParser(CommandFactory<?, CommandSource> commandFactory) {

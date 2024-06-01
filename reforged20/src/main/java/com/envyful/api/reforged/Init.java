89001a294d4442d13e22d7b1ba5f064236898c2e
Daniel Norris <dnorris443@gmail.com>
2024-06-01 16:58:18 +0100

    ref(reforged20): javadocs

--- reforged20/src/main/java/com/envyful/api/reforged/Init.java
+++ reforged20/src/main/java/com/envyful/api/reforged/Init.java
@@ -5,6 +5,12 @@ import com.envyful.api.forge.InitializationTask;
 import com.envyful.api.reforged.config.PokemonSpecSerializer;
 import com.pixelmonmod.api.pokemon.PokemonSpecification;
 
+/**
+ *
+ * Task that runs on server startup to register the PokemonSpecSerializer before the configs are loaded
+ * by the plugin or mod
+ *
+ */
 public class Init implements InitializationTask {
 
     @Override

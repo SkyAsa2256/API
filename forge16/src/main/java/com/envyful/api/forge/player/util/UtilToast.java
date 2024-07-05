a7d89d9388be3042ca7432283fa4ceaacf62a00d
Daniel Norris <dnorris443@gmail.com>
2024-07-05 16:07:38 +0100

    feat(api): add UtilToast for forge16 & forge20

---
+++ forge16/src/main/java/com/envyful/api/forge/player/util/UtilToast.java
@@ -0,0 +1,98 @@
+package com.envyful.api.forge.player.util;
+
+import com.google.common.base.Preconditions;
+import net.minecraft.advancements.*;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SAdvancementInfoPacket;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * UtilToast -
+ * A class for sending toast messages to players.
+ *
+ */
+public class UtilToast {
+
+    private static final ResourceLocation TESTER = new ResourceLocation("mia", "tester");
+
+    public static void sendToast(ServerPlayerEntity player, FrameType frameType, ITextComponent message, ItemStack display) {
+        var displayInfo = new DisplayInfo(
+                display,
+                message, // \n indicates a new line
+                new StringTextComponent(""), // I think this only gets used in the advancement display UI
+                null, // This is only used in the advancement display UI
+                frameType, // FrameType.CHALLENGE, FrameType.GOAL, FrameType.TASK changes the text displayed at the top (if a single line) or at the start (if multiline)
+                true, // Whether to show a toast message
+                false, // Whether to announce in chat - I don't think this matters here?
+                true // Whether to hide the advancement in the advancement display UI
+        );
+
+        var advancement = new Advancement(
+                TESTER, null,
+                displayInfo,
+                AdvancementRewards.EMPTY,
+                Map.of("test", new Criterion()),
+                new String[][]{{"test"}}
+        );
+
+        var progress = new AdvancementProgress();
+        progress.update(
+                Map.of("test", new Criterion()),
+                new String[][]{{"test"}}
+        );
+
+        progress.grantProgress("test");
+
+        player.connection.send(new SAdvancementInfoPacket(
+                false, List.of(advancement), Set.of(), Map.of(
+                advancement.getId(), progress
+        )));
+    }
+
+    public static ToastBuilder builder() {
+        return new ToastBuilder();
+    }
+
+    public static class ToastBuilder {
+
+        private ITextComponent message;
+        private ItemStack display;
+        private FrameType frameType;
+
+        public ToastBuilder message(ITextComponent message) {
+            this.message = message;
+            return this;
+        }
+
+        public ToastBuilder display(ItemStack display) {
+            this.display = display;
+            return this;
+        }
+
+        public ToastBuilder frameType(FrameType frameType) {
+            this.frameType = frameType;
+            return this;
+        }
+
+        public void send(ServerPlayerEntity... players) {
+            Preconditions.checkNotNull(message, "Message cannot be null");
+            Preconditions.checkNotNull(display, "Display cannot be null");
+            Preconditions.checkNotNull(frameType, "FrameType cannot be null");
+
+            for (var player : players) {
+                if (player == null) {
+                    continue;
+                }
+                UtilToast.sendToast(player, frameType, message, display);
+            }
+        }
+    }
+}
---
+++ forge20/src/main/java/com/envyful/api/forge/player/util/UtilToast.java
@@ -0,0 +1,97 @@
+package com.envyful.api.forge.player.util;
+
+import com.google.common.base.Preconditions;
+import net.minecraft.advancements.*;
+import net.minecraft.advancements.critereon.ImpossibleTrigger;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundUpdateAdvancementsPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ *
+ * UtilToast -
+ * A class for sending toast messages to players.
+ *
+ */
+public class UtilToast {
+
+    private static final ResourceLocation TESTER = new ResourceLocation("mia", "tester");
+
+    public static void sendToast(ServerPlayer player, FrameType frameType, Component message, ItemStack display) {
+        var displayInfo = new DisplayInfo(
+                display,
+                message, // \n indicates a new line
+               Component.empty(), // I think this only gets used in the advancement display UI
+                null, // This is only used in the advancement display UI
+                frameType, // FrameType.CHALLENGE, FrameType.GOAL, FrameType.TASK changes the text displayed at the top (if a single line) or at the start (if multiline)
+                true, // Whether to show a toast message
+                false, // Whether to announce in chat - I don't think this matters here?
+                true // Whether to hide the advancement in the advancement display UI
+        );
+
+        var advancement = new Advancement(
+                Optional.empty(),
+                Optional.of(displayInfo),
+                AdvancementRewards.EMPTY,
+                Map.of("test", new Criterion<>(CriteriaTriggers.IMPOSSIBLE, new ImpossibleTrigger.TriggerInstance())),
+                new AdvancementRequirements(new String[][]{{"test"}}),
+                false
+        );
+
+        var progress = new AdvancementProgress();
+        progress.update(new AdvancementRequirements(new String[][]{{"test"}}));
+
+        progress.grantProgress("test");
+
+        player.connection.send(new ClientboundUpdateAdvancementsPacket(
+                false, List.of(new AdvancementHolder(TESTER, advancement)), Set.of(), Map.of(
+                TESTER, progress
+        )));
+    }
+
+    public static ToastBuilder builder() {
+        return new ToastBuilder();
+    }
+
+    public static class ToastBuilder {
+
+        private Component message;
+        private ItemStack display;
+        private FrameType frameType;
+
+        public ToastBuilder message(Component message) {
+            this.message = message;
+            return this;
+        }
+
+        public ToastBuilder display(ItemStack display) {
+            this.display = display;
+            return this;
+        }
+
+        public ToastBuilder frameType(FrameType frameType) {
+            this.frameType = frameType;
+            return this;
+        }
+
+        public void send(ServerPlayer... players) {
+            Preconditions.checkNotNull(message, "Message cannot be null");
+            Preconditions.checkNotNull(display, "Display cannot be null");
+            Preconditions.checkNotNull(frameType, "FrameType cannot be null");
+
+            for (var player : players) {
+                if (player == null) {
+                    continue;
+                }
+                UtilToast.sendToast(player, frameType, message, display);
+            }
+        }
+    }
+}

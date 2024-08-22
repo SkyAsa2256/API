7599c8e85af84cf5c33644b92ba1ea1871251671
Daniel Norris <dnorris443@gmail.com>
2024-08-22 12:43:39 +0100

    fix(reforged16/20): update ConfigBattleRule#with to work with Battle Clauses

--- reforged20/src/main/java/com/envyful/api/reforged/battle/ConfigBattleRule.java
+++ reforged20/src/main/java/com/envyful/api/reforged/battle/ConfigBattleRule.java
@@ -1,8 +1,15 @@
 package com.envyful.api.reforged.battle;
 
+import com.envyful.api.concurrency.UtilLogger;
+import com.google.common.collect.Sets;
 import com.pixelmonmod.pixelmon.battles.api.rules.BattleRuleRegistry;
 import com.pixelmonmod.pixelmon.battles.api.rules.BattleRules;
+import com.pixelmonmod.pixelmon.battles.api.rules.clauses.BattleClause;
+import com.pixelmonmod.pixelmon.battles.api.rules.clauses.BattleClauseRegistry;
+import com.pixelmonmod.pixelmon.battles.api.rules.value.ClausesValue;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.util.Set;
 
 /**
  *
@@ -32,6 +39,22 @@ public class ConfigBattleRule {
     }
 
     public BattleRules with(BattleRules battleRules) {
+        var property = BattleRuleRegistry.getProperty(this.battleRuleType);
+        Set<BattleClause> clauses = Sets.newHashSet(battleRules.getClauseList());
+
+        if (property != null) {
+            battleRules.set(property, this.battleRuleValue);
+        } else {
+            var clause = BattleClauseRegistry.getClause(this.battleRuleType);
+            if (clause != null) {
+                clauses.add(clause);
+            } else {
+                UtilLogger.logger().ifPresent(logger -> logger.error("Invalid battle rule or clause found `{}`", this.battleRuleType));
+            }
+        }
+
+        battleRules.set(BattleRuleRegistry.CLAUSES, new ClausesValue(clauses));
+
         return battleRules.set(BattleRuleRegistry.getProperty(this.battleRuleType), this.battleRuleValue);
     }
 }

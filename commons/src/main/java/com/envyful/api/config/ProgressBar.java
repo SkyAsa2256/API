67947f47c5e66d0a4ebaa5d128e573a42ad3eb5d
Daniel Norris <dnorris443@gmail.com>
2024-09-17 14:31:00 +0100

    feat(api): add ProgressBar config class

---
+++ commons/src/main/java/com/envyful/api/config/ProgressBar.java
@@ -0,0 +1,31 @@
+package com.envyful.api.config;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigSerializable
+public class ProgressBar {
+
+    private String progressBar = "&7[&a%filled%&7%empty%&7]";
+    private String filledProgressBar = "&a|";
+    private String emptyProgressBar = "&c|";
+    private int progressBarLength = 10;
+
+    public ProgressBar() {
+    }
+
+    public ProgressBar(String progressBar, String filledProgressBar, String emptyProgressBar, int progressBarLength) {
+        this.progressBar = progressBar;
+        this.filledProgressBar = filledProgressBar;
+        this.emptyProgressBar = emptyProgressBar;
+        this.progressBarLength = progressBarLength;
+    }
+
+    public String createProgressBar(double percentage) {
+        int filled = (int) Math.round(percentage * this.progressBarLength);
+        int empty = this.progressBarLength - filled;
+
+        return this.progressBar
+                .replace("%filled%", this.filledProgressBar.repeat(filled))
+                .replace("%empty%", this.emptyProgressBar.repeat(empty));
+    }
+}

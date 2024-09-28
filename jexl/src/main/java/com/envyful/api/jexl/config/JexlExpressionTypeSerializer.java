d35f5dd98eaa138bca22f5bf100215bbfad2c86c
Daniel Norris <dnorris443@gmail.com>
2024-09-28 09:29:31 +0100

    feat(jexl): add JexlExpressionTypeSerializer

---
+++ jexl/src/main/java/com/envyful/api/jexl/config/JexlExpressionTypeSerializer.java
@@ -0,0 +1,22 @@
+package com.envyful.api.jexl.config;
+
+import com.envyful.api.jexl.UtilJexl;
+import org.apache.commons.jexl3.JexlExpression;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.serialize.SerializationException;
+import org.spongepowered.configurate.serialize.TypeSerializer;
+
+import java.lang.reflect.Type;
+
+public class JexlExpressionTypeSerializer implements TypeSerializer<JexlExpression> {
+    @Override
+    public JexlExpression deserialize(Type type, ConfigurationNode node) throws SerializationException {
+        return UtilJexl.getEngine().createExpression(node.getString());
+    }
+
+    @Override
+    public void serialize(Type type, @Nullable JexlExpression obj, ConfigurationNode node) throws SerializationException {
+        node.set(obj.getSourceText());
+    }
+}

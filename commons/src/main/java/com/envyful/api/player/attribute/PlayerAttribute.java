6ccc398abe82dc754d3e28150f2a92f882fb23eb
Daniel Norris <dnorris443@gmail.com>
2024-09-30 15:05:57 +0100

    fix(api): remove references to old classes

--- commons/src/main/java/com/envyful/api/player/attribute/PlayerAttribute.java
+++ commons/src/main/java/com/envyful/api/player/attribute/PlayerAttribute.java
@@ -1,17 +1,9 @@
 package com.envyful.api.player.attribute;
 
-import com.envyful.api.concurrency.UtilLogger;
 import com.envyful.api.database.Database;
 import com.envyful.api.player.EnvyPlayer;
-import com.envyful.api.player.attribute.data.TableData;
 import com.envyful.api.player.save.SaveManager;
-import com.envyful.api.player.save.attribute.DataDirectory;
-import com.envyful.api.player.save.impl.JsonSaveManager;
-import com.envyful.api.player.save.impl.SQLSaveManager;
-import com.google.common.collect.Lists;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -68,45 +60,10 @@ public abstract class PlayerAttribute<A, B extends EnvyPlayer<C>, C>
 
     @Override
     public void deleteAll(SaveManager<?> saveManager) {
-        this.findTargets(saveManager).forEach(s -> {
-            if (this.getDatabase() == null) {
-                saveManager.delete(s);
-            } else {
-                saveManager.delete(this.getDatabase(), s);
-            }
-        });
+        //TODO:
     }
 
     protected Database getDatabase() {
         return this.database;
-    }
-
-    protected List<String> findTargets(SaveManager<?> saveManager) {
-        List<String> targets = Lists.newArrayList();
-
-        if (saveManager instanceof SQLSaveManager) {
-            TableData tableData = this.getClass().getAnnotation(TableData.class);
-
-            if (tableData == null) {
-                UtilLogger.logger().ifPresent(logger -> logger.error("No table data annotation found for {}", this.getClass().getName()));
-                return Collections.emptyList();
-            }
-
-            targets.addAll(Lists.newArrayList(tableData.value()));
-        } else if (saveManager instanceof JsonSaveManager) {
-            DataDirectory dataDirectory = this.getClass().getAnnotation(DataDirectory.class);
-
-            if (dataDirectory == null) {
-                UtilLogger.logger().ifPresent(logger -> logger.error("No data directory annotation found for {}", this.getClass().getName()));
-                return Collections.emptyList();
-            }
-
-            targets.addAll(Lists.newArrayList(dataDirectory.value()));
-        } else {
-            UtilLogger.logger().ifPresent(logger -> logger.error("Unknown save manager type {}", saveManager.getClass().getName()));
-            return Collections.emptyList();
-        }
-
-        return targets;
     }
 }

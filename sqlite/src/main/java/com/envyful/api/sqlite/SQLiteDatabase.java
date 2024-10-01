bc692b7710d4c3224ebd08a1389ebf4eb6da81fc
Daniel Norris <dnorris443@gmail.com>
2024-10-01 11:30:45 +0100

    feat(sqlite): update to use hikari

--- sqlite/src/main/java/com/envyful/api/sqlite/SQLiteDatabase.java
+++ sqlite/src/main/java/com/envyful/api/sqlite/SQLiteDatabase.java
@@ -1,35 +1,51 @@
 package com.envyful.api.sqlite;
 
-import com.envyful.api.concurrency.UtilLogger;
 import com.envyful.api.database.Database;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
 
 public class SQLiteDatabase implements Database {
 
-    private final Connection connection;
+    private final HikariDataSource hikari;
 
-    public SQLiteDatabase(String file) throws SQLException {
-        this.connection = DriverManager.getConnection("jdbc:sqlite:" + file);
+    public SQLiteDatabase(String filePath) {
+        var config = new HikariConfig();
+
+        config.setMaximumPoolSize(1);
+        config.setPoolName("SQLITE");
+        config.setJdbcUrl("jdbc:sqlite:" + filePath);
+        config.addDataSourceProperty("databaseName", "envyware");
+        config.addDataSourceProperty("cachePrepStmts", true);
+        config.addDataSourceProperty("prepStmtCacheSize", 250);
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+        config.addDataSourceProperty("useServerPrepStmts", true);
+        config.addDataSourceProperty("cacheCallableStmts", true);
+        config.addDataSourceProperty("alwaysSendSetIsolation", false);
+        config.addDataSourceProperty("cacheServerConfiguration", true);
+        config.addDataSourceProperty("elideSetAutoCommits", true);
+        config.addDataSourceProperty("useLocalSessionState", true);
+        config.addDataSourceProperty("characterEncoding","utf8");
+        config.addDataSourceProperty("useUnicode","true");
+        config.addDataSourceProperty("maxLifetime", TimeUnit.SECONDS.toMillis(30));
+        config.setMaxLifetime(TimeUnit.SECONDS.toMillis(30));
+        config.setConnectionTimeout(TimeUnit.SECONDS.toMillis(30));
+        config.setLeakDetectionThreshold(TimeUnit.SECONDS.toMillis(60));
+        config.setConnectionTestQuery("/* Ping */ SELECT 1");
+
+        this.hikari = new HikariDataSource(config);
     }
 
     @Override
-    public Connection getConnection() throws SQLException, UnsupportedOperationException {
-        if (this.connection == null) {
-            throw new SQLException("Failed to connect to database");
-        }
-
-        return this.connection;
+    public Connection getConnection() throws SQLException {
+        return this.hikari.getConnection();
     }
 
     @Override
     public void close() {
-        try {
-            this.connection.close();
-        } catch (SQLException e) {
-            UtilLogger.logger().ifPresent(logger -> logger.error("Failed to close connection", e));
-        }
+        this.hikari.close();
     }
 }

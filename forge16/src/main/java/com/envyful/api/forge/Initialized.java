178cb4bae9ef2ca51541aca6c7c68ab7805cd7ee
Daniel Norris <dnorris443@gmail.com>
2024-10-08 16:46:57 +0100

    ref(api): javadoc Initialized.java

--- forge16/src/main/java/com/envyful/api/forge/Initialized.java
+++ forge16/src/main/java/com/envyful/api/forge/Initialized.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *
+ * Annotation to mark a class as being called when the mod is initialized
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Initialized {

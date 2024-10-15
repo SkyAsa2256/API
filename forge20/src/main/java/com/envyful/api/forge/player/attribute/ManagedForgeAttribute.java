e692a517464a9630740032edab4f7e8c53691e85
Daniel Norris <dnorris443@gmail.com>
2024-10-15 11:37:26 +0100

    ref(api): javadoc ManagedForgeAttribute

--- forge20/src/main/java/com/envyful/api/forge/player/attribute/ManagedForgeAttribute.java
+++ forge20/src/main/java/com/envyful/api/forge/player/attribute/ManagedForgeAttribute.java
@@ -4,6 +4,12 @@ import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.player.attribute.PlayerAttribute;
 import net.minecraft.server.level.ServerPlayer;
 
+/**
+ *
+ * Abstract class for Forge attributes
+ *
+ * @param <A> The type of the attribute
+ */
 public abstract class ManagedForgeAttribute<A>
         extends PlayerAttribute<A, ForgeEnvyPlayer, ServerPlayer> {
 

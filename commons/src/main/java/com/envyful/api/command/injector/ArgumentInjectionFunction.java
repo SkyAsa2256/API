a1bb529f6b2dca053a1be13c31d25aa2d265ab61
Daniel Norris <dnorris443@gmail.com>
2024-11-09 16:28:06 +0000

    ref(api): javadoc ArgumentInjectionFunction

--- commons/src/main/java/com/envyful/api/command/injector/ArgumentInjectionFunction.java
+++ commons/src/main/java/com/envyful/api/command/injector/ArgumentInjectionFunction.java
@@ -3,6 +3,13 @@ package com.envyful.api.command.injector;
 import java.lang.annotation.Annotation;
 import java.util.List;
 
+/**
+ *
+ * A functional interface for injecting arguments into a command
+ *
+ * @param <A> The type of the argument being injected
+ * @param <B> The type of the sender
+ */
 public interface ArgumentInjectionFunction<A, B> {
 
     A apply(B sender, List<Annotation> annotations, String[] args);

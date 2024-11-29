dd758cdf1bfd3f844862090e344b415326feb09d
Daniel Norris <dnorris443@gmail.com>
2024-11-29 14:42:26 +0000

    feat(api): add h2 database support

---
+++ h2/src/main/java/com/envyful/api/sqlite/H2Database.java
@@ -0,0 +1,67 @@
+package com.envyful.api.sqlite;
+
+import com.envyful.api.concurrency.UtilLogger;
+import com.envyful.api.database.Database;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * SQLite implementation of the {@link Database} interface
+ *
+ */
+public class H2Database implements Database {
+
+    private final HikariDataSource hikari;
+
+    public H2Database(String filePath) {
+        this.loadDriver();
+
+        var config = new HikariConfig();
+
+        config.setMaximumPoolSize(1);
+        config.setPoolName("H2");
+        config.setJdbcUrl("jdbc:h2:" + filePath);
+        config.addDataSourceProperty("databaseName", "envyware");
+        config.addDataSourceProperty("cachePrepStmts", true);
+        config.addDataSourceProperty("prepStmtCacheSize", 250);
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+        config.addDataSourceProperty("useServerPrepStmts", true);
+        config.addDataSourceProperty("cacheCallableStmts", true);
+        config.addDataSourceProperty("alwaysSendSetIsolation", false);
+        config.addDataSourceProperty("cacheServerConfiguration", true);
+        config.addDataSourceProperty("elideSetAutoCommits", true);
+        config.addDataSourceProperty("useLocalSessionState", true);
+        config.addDataSourceProperty("characterEncoding","utf8");
+        config.addDataSourceProperty("useUnicode","true");
+        config.addDataSourceProperty("maxLifetime", TimeUnit.SECONDS.toMillis(30));
+        config.setMaxLifetime(TimeUnit.SECONDS.toMillis(30));
+        config.setConnectionTimeout(TimeUnit.SECONDS.toMillis(30));
+        config.setLeakDetectionThreshold(TimeUnit.SECONDS.toMillis(60));
+        config.setConnectionTestQuery("/* Ping */ SELECT 1");
+
+        this.hikari = new HikariDataSource(config);
+    }
+
+    private void loadDriver() {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            UtilLogger.logger().ifPresent(logger -> logger.error("Failed to load H2 driver"));
+        }
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return this.hikari.getConnection();
+    }
+
+    @Override
+    public void close() {
+        this.hikari.close();
+    }
+}
---
+++ h2/src/main/java/com/envyful/api/sqlite/config/H2DatabaseDetailsConfig.java
@@ -0,0 +1,57 @@
+package com.envyful.api.sqlite.config;
+
+import com.envyful.api.concurrency.UtilLogger;
+import com.envyful.api.config.database.DatabaseDetailsConfig;
+import com.envyful.api.config.database.DatabaseDetailsRegistry;
+import com.envyful.api.database.Database;
+import com.envyful.api.sqlite.H2Database;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.io.File;
+import java.io.IOException;
+
+@ConfigSerializable
+public class H2DatabaseDetailsConfig implements DatabaseDetailsConfig {
+
+    public static final String ID = "h2";
+
+    private String filePath;
+
+    public H2DatabaseDetailsConfig(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public H2DatabaseDetailsConfig() {
+
+    }
+
+    public static void register() {
+        DatabaseDetailsRegistry.register(ID, H2DatabaseDetailsConfig.class);
+    }
+
+    @Override
+    public Database createDatabase() {
+        File file = new File(this.filePath);
+
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                UtilLogger.logger().ifPresent(logger -> logger.error("Failed to create SQLite database file", e));
+                return null;
+            }
+        }
+
+        try {
+            return new H2Database(this.filePath);
+        } catch (Exception e) {
+            UtilLogger.logger().ifPresent(logger -> logger.error("Failed to create SQLite database", e));
+        }
+
+        return null;
+    }
+}

700753b01d3150746ac89145d28d8dbde3b93e05
Daniel Norris <dnorris443@gmail.com>
2024-11-29 04:39:25 +0000

    feat(api): add ComposedAttributeTrigger

---
+++ commons/src/main/java/com/envyful/api/player/attribute/trigger/ComposedAttributeTrigger.java
@@ -0,0 +1,30 @@
+package com.envyful.api.player.attribute.trigger;
+
+import com.envyful.api.player.EnvyPlayer;
+import com.envyful.api.player.PlayerManager;
+import com.envyful.api.player.attribute.AttributeTrigger;
+
+import java.util.List;
+
+public class ComposedAttributeTrigger<T> implements AttributeTrigger<T> {
+
+    private final List<AttributeTrigger<T>> triggers;
+
+    public ComposedAttributeTrigger(List<AttributeTrigger<T>> triggers) {
+        this.triggers = triggers;
+    }
+
+    @Override
+    public void addAttribute(PlayerManager.AttributeData<?, ?, T> attribute) {
+        for (AttributeTrigger<T> trigger : this.triggers) {
+            trigger.addAttribute(attribute);
+        }
+    }
+
+    @Override
+    public void trigger(EnvyPlayer<T> player) {
+        for (AttributeTrigger<T> trigger : this.triggers) {
+            trigger.trigger(player);
+        }
+    }
+}

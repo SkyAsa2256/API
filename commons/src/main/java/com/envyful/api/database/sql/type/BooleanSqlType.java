8f85008e0ec753eb3a9f1a35b181e17d8f049f0a
Daniel Norris <dnorris443@gmail.com>
2024-12-13 23:00:30 +0000

    ref(api): javadoc BooleanSqlType

--- commons/src/main/java/com/envyful/api/database/sql/type/BooleanSqlType.java
+++ commons/src/main/java/com/envyful/api/database/sql/type/BooleanSqlType.java
@@ -5,6 +5,11 @@ import com.envyful.api.database.sql.SingleValuedSqlType;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ *
+ * A SQL type handler for a boolean
+ *
+ */
 public class BooleanSqlType extends SingleValuedSqlType<Boolean> {
 
     public BooleanSqlType(Boolean value) {

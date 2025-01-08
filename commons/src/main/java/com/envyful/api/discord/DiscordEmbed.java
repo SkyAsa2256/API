a0093a8f9ae9e3b89bfcc89e7126bd37d3c0f9d6
Daniel Norris <dnorris443@gmail.com>
2025-01-08 17:34:46 +0000

    fix(api): add missing import for DiscordEmbed

--- commons/src/main/java/com/envyful/api/discord/DiscordEmbed.java
+++ commons/src/main/java/com/envyful/api/discord/DiscordEmbed.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;

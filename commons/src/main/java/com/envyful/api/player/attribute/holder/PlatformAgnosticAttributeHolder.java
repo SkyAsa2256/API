934ef4d6b2b32e95e21fc40feb58a881c9fc6c9a
Daniel Norris <dnorris443@gmail.com>
2025-01-08 15:04:00 +0000

    fix(api): make PlatformAgnosticAttributeHolder abstract

--- commons/src/main/java/com/envyful/api/player/attribute/holder/PlatformAgnosticAttributeHolder.java
+++ commons/src/main/java/com/envyful/api/player/attribute/holder/PlatformAgnosticAttributeHolder.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-public class PlatformAgnosticAttributeHolder implements AttributeHolder {
+public abstract class PlatformAgnosticAttributeHolder implements AttributeHolder {
 
     protected final Map<Class<?>, AttributeInstance<?, ?>> attributes = new HashMap<>();
 

440afccfcb930ffdc50f2210c777532fd2994778
Daniel Norris <dnorris443@gmail.com>
2025-02-09 08:12:12 +0000

    ref(api): change UtilLogger to not use Optionals

--- commons/src/main/java/com/envyful/api/concurrency/UtilLogger.java
+++ commons/src/main/java/com/envyful/api/concurrency/UtilLogger.java
@@ -1,5 +1,6 @@
 package com.envyful.api.concurrency;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
@@ -9,15 +10,18 @@ import java.util.Optional;
  * Static utility class used by mods, and plugins, to prove a logging
  * interface for the generic API classes found in this repository.
  * <br>
- * Best practice is to use the {@link UtilLogger#logger()} method
+ * Best practice is to use the {@link UtilLogger#getLogger()} method
  * as it is not guaranteed that every mod will provide a logger to the API
- * and not using that could result in a {@link NullPointerException}
- * in the code you're attempting to run
+ * and so it will default to the "EnvyAPI" logger when no logger is provided
  *
  */
 public class UtilLogger {
 
+    private static final Logger LOGGER = LogManager.getLogger("EnvyAPI");
+
     private static Logger logger;
+
+    private UtilLogger() {}
 
     /**
      *
@@ -34,8 +38,30 @@ public class UtilLogger {
      * Safely attempts to get the logger provided by the mod/plugin
      *
      * @return The logger, if it exists
+     * @deprecated Use {@link UtilLogger#getLogger()} instead
      */
+    @Deprecated(since = "7.5.9", forRemoval = true)
     public static Optional<Logger> logger() {
+        if (logger == null) {
+            return Optional.of(LOGGER);
+        }
+
         return Optional.ofNullable(logger);
+    }
+
+    /**
+     *
+     * Safely attempts to get the logger provided by the mod/plugin
+     * <br>
+     * If the logger has not been set, it will return the API's default logger
+     *
+     * @return The logger, if it exists
+     */
+    public static Logger getLogger() {
+        if (logger == null) {
+            return LOGGER;
+        }
+
+        return logger;
     }
 }

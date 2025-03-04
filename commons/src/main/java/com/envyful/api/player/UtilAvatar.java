b148178c446b8ec1832ac3b9ce5d02d79de687fd
Daniel Norris <dnorris443@gmail.com>
2025-03-04 16:51:20 +0000

    ref(api): javadoc UtilAvatar

--- commons/src/main/java/com/envyful/api/player/UtilAvatar.java
+++ commons/src/main/java/com/envyful/api/player/UtilAvatar.java
@@ -9,9 +9,24 @@ import java.util.UUID;
  **/
 public class UtilAvatar {
 
+    /**
+     *
+     * Gets the avatar URL of the player
+     *
+     * @param player The player to get the avatar URL of
+     * @return The URL of the player's avatar
+     */
     public static String getAvatarUrl(EnvyPlayer<?> player) {
         return "https://crafatar.com/avatars/{uuid-nodashes}.png?size=128".replace("{uuid-nodashes}", player.getUniqueId().toString().replace("-", ""));
     }
+
+    /**
+     *
+     * Gets the avatar URL of the player
+     *
+     * @param player The uuid of the player to get the avatar URL of
+     * @return The URL of the player's avatar
+     */
     public static String getAvatarUrl(UUID player) {
         return "https://crafatar.com/avatars/{uuid-nodashes}.png?size=128".replace("{uuid-nodashes}", player.toString().replace("-", ""));
     }

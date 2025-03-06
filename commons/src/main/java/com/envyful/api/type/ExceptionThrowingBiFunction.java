6de458ab0c5c3e6cc34a570fb8666061b78d0820
Daniel Norris <dnorris443@gmail.com>
2025-03-06 16:52:44 +0000

    ref(api): javadoc ExceptionThrowingBiFunction

--- commons/src/main/java/com/envyful/api/type/ExceptionThrowingBiFunction.java
+++ commons/src/main/java/com/envyful/api/type/ExceptionThrowingBiFunction.java
@@ -1,5 +1,14 @@
 package com.envyful.api.type;
 
+/**
+ *
+ * Functional interface for a bi-function {@link java.util.function.BiFunction} that can throw a checked exception
+ *
+ * @param <A> The first parameter type
+ * @param <B> The second parameter type
+ * @param <C> The return type
+ * @param <D> The exception type
+ */
 @FunctionalInterface
 public interface ExceptionThrowingBiFunction<A, B, C, D extends Throwable> {
 

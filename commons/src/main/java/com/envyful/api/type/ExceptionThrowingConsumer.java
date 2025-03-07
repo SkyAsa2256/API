046ec23b289161c872e17fa7d0e1d8f20642b0ec
Daniel Norris <dnorris443@gmail.com>
2025-03-07 16:53:09 +0000

    ref(api): javadoc ExceptionThrowingConsumer

--- commons/src/main/java/com/envyful/api/type/ExceptionThrowingConsumer.java
+++ commons/src/main/java/com/envyful/api/type/ExceptionThrowingConsumer.java
@@ -1,5 +1,12 @@
 package com.envyful.api.type;
 
+/**
+ *
+ * Functional interface for a consumer {@link java.util.function.Consumer} that throws a checked exception
+ *
+ * @param <T> The type of the input to the operation
+ * @param <D> The type of the exception that can be thrown
+ */
 @FunctionalInterface
 public interface ExceptionThrowingConsumer<T, D extends Throwable> {
 

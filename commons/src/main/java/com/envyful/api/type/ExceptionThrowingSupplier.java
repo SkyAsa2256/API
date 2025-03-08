95ad8de9bceeda9468c27ad008939d6c3f01247f
Daniel Norris <dnorris443@gmail.com>
2025-03-08 16:53:30 +0000

    ref(api): javadoc ExceptionThrowingSupplier.java

--- commons/src/main/java/com/envyful/api/type/ExceptionThrowingSupplier.java
+++ commons/src/main/java/com/envyful/api/type/ExceptionThrowingSupplier.java
@@ -1,5 +1,12 @@
 package com.envyful.api.type;
 
+/**
+ *
+ * Functional interface for a supplier {@link java.util.function.Supplier} that can throw a checked exception
+ *
+ * @param <T> The type of the object being supplied
+ * @param <D> The type of the exception being thrown
+ */
 @FunctionalInterface
 public interface ExceptionThrowingSupplier<T, D extends Throwable> {
 
